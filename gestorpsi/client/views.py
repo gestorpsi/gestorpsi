bf8e8ed78113be830e151fdbc9696036d60545bb
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-07-15 14:17:19 +0000

    created unique method for save/update clients
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@22 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/client/views.py
+++ gestorpsi/client/views.py
@@ -1,54 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django import newforms as forms
 from gestorpsi.client.models import Client, Phone, ClientForm, PhoneForm
-
-def index(request): 
-    return render_to_response('client/client_index.html', {'clientList': Client.objects.all().filter(active = True) })
-
-def form(request, client_id=0):
-    phones = []
-    try:
-        client = get_object_or_404(Client, pk=client_id)
-        for phone in client.phone_set.all():
-            phones.append(phone)
-    except:
-        client = Client()
-    return render_to_response('client/client_form.html', {'client': client, 'phones': phones } )
-    
-
-def save(request):
-    client = Client(name= request.POST['name'],email= request.POST['email'],birthDate= request.POST['birthDate'])
-    client.save()
-    phones_area   = request.POST.getlist('area')
-    phones_number = request.POST.getlist('phoneNumber')
-    phones_exts   = request.POST.getlist('ext')
-    total = len(phones_number)
-    for i in range(0, total):
-        if (len(phones_number[i])):
-            client.phone_set.create(area=phones_area[i],phoneNumber=phones_number[i],ext=phones_exts[i])
-    return render_to_response('client/client_save.html', {'result': "OK"})
-
-def update(request, client_id):
-    client = get_object_or_404(Client, pk=client_id)
-    client.name = request.POST['name']
-    client.email = request.POST['email']
-    client.birthDate = request.POST['birthDate']
-    client.save()
-    client.ob
-    phones = phoneList(request.POST.getlist('area'), request.POST.getlist('phoneNumber'), request.POST.getlist('ext'))
-    for p in phones:
-        print p
- 
-
-    #FormCliente = forms.form_for_model(Cliente)
-    #formCliente = FormCliente(request.POST)
-    #if formCliente.is_valid():
-    #    formCliente.save()
-    #    return render_to_response('cliente_save.txt', {'resultado': "OK"})
-    #else:
-    #    return render_to_response('cliente_add.txt', {'formCliente': formCliente, 'resultado': "PROBLEMAS DE VALIDACAO"})
-    return HttpResponse("Alteracao do Cliente %s efetuada com sucesso." % client.name)
 
 def phoneList(areas, numbers, exts):
     total = len(numbers)
@@ -57,6 +10,34 @@ def phoneList(areas, numbers, exts):
         if (len(numbers[i])): 
             phones.append(Phone(area=areas[i],phoneNumber=numbers[i],ext=exts[i]))
     return phones
+
+def index(request): 
+    return render_to_response('client/client_index.html', {'clientList': Client.objects.all().filter(active = True) })
+
+def form(request, client_id=0):
+    try:
+        phones = []
+        client = get_object_or_404(Client, pk=client_id)
+        for phone in client.phone_set.all():
+            phones.append(phone)
+    except:
+        client = Client()
+    return render_to_response('client/client_form.html', {'client': client, 'phones': phones } )
+
+def save(request, client_id=0):
+    try:
+        client = get_object_or_404(Client, pk=client_id)
+    except Http404:
+        client = Client()
+    client.name = request.POST['name']
+    client.email = request.POST['email']
+    client.birthDate = request.POST['birthDate']
+    client.save()
+    client.phone_set.all().delete()
+    for phone in phoneList(request.POST.getlist('area'), request.POST.getlist('phoneNumber'), request.POST.getlist('ext')):
+        phone.client = client
+        phone.save()
+    return render_to_response('client/client_index.html', {'clientList': Client.objects.all().filter(active = True) })
 
 def delete(request, client_id):
     client = get_object_or_404(Client, pk=client_id)

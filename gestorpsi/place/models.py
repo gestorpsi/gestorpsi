c506f2cb4450c8b4a9968c4b8254a209d7c98e5a
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-07-16 15:13:33 +0000

    app places moved to place
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@27 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

---
+++ gestorpsi/place/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.newforms import ModelForm
+import unittest
+
+class Address( models.Model ):
+   street= models.CharField( max_length= 200 )
+   city= models.CharField( max_length= 80 )
+
+   def __unicode__(self):
+      return "Street: %s, City: %s" % ( self.street, self.city )
+
+   class Admin:
+      pass
+
+
+class Place( models.Model ):
+   description= models.CharField( max_length= 80 )
+   visible= models.BooleanField()
+   address= models.ForeignKey( Address ) 
+
+   def __unicode__(self):
+      return "%s" % self.description
+
+   class Admin:
+      pass
+
+class PlaceForm(ModelForm):
+    class Meta:
+        model= Place
+
+
+class RoomType( models.Model ):
+   description= models.CharField( max_length= 45, unique= True )
+   
+   def __unicode__(self):
+      return "%s" % self.description
+
+   class Admin:
+      pass
+
+
+class Room( models.Model ):
+   description= models.CharField( max_length= 80 )
+   size= models.IntegerField()
+   place= models.ForeignKey( Place, related_name='place' )
+
+   def __unicode__(self):
+      return "%s" % self.description
+
+   class Admin:
+      pass
+  
+class RoomForm(ModelForm):
+      class Meta:
+          model= Room
+

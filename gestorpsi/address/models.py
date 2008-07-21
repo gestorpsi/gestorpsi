ebedbe39a107bb8c38b84d20dcd76e913c68a003
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-07-21 20:15:55 +0000

    added unicode symbols to return's added 'addressNumber' field on address model
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@53 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/address/models.py
+++ gestorpsi/address/models.py
@@ -6,7 +6,7 @@ class Country(models.Model):
     name = models.CharField(max_length=50)
     nationality = models.CharField(max_length=50)
     def __unicode__(self):
-        return self.name
+        return u"%s" % self.name
     class Meta:
         ordering = ['name']
 
@@ -15,7 +15,7 @@ class State(models.Model):
     shortName = models.CharField(max_length=2)
     country = models.ForeignKey(Country)
     def __unicode__(self):
-        return self.name
+        return u"%s" % self.name
     class Meta:
         ordering = ['name']
 
@@ -23,23 +23,23 @@ class City(models.Model):
     name = models.CharField(max_length=50)
     state = models.ForeignKey(State)
     def __unicode__(self):
-        return self.name
+        return u"%s" % self.name
     class Meta:
         ordering = ['name']
 
 class AddressType(models.Model):
     description = models.CharField(max_length=20)
     def __unicode__(self):
-        return self.description
+        return u"%s" % self.description
     class Admin: pass
 
 class Address(models.Model):
-    # Falta colocar NUMERO da casa
     addressPrefix = models.CharField(max_length=10)
-    addressLine1 = models.CharField(max_length=50)
-    addressLine2 = models.CharField(max_length=50)
-    neighborhood = models.CharField(max_length=30)
-    zipCode = models.CharField(max_length=10)
+    addressLine1 = models.CharField(max_length=50, blank=True)
+    addressLine2 = models.CharField(max_length=50, blank=True)
+    addressNumber = models.CharField(max_length=10, blank=True)
+    neighborhood = models.CharField(max_length=30, blank=True)
+    zipCode = models.CharField(max_length=10, blank=True)
     addressType = models.ForeignKey(AddressType)
     city = models.ForeignKey(City)
     # Generic Relationship
@@ -47,4 +47,4 @@ class Address(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
     def __unicode__(self):
-        return "%s %s\n%s" % (self.addressPrefix, self.addressLine1, self.addressLine2)
+        return u"%s %s\n%s" % (self.addressPrefix, self.addressLine1, self.addressLine2)

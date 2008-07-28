de017bac498328a7af2e74437a27b1ad27d12e66
danilo <danilo@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-07-28 17:51:22 +0000

    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@90 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/client/views.py
+++ gestorpsi/client/views.py
@@ -37,40 +37,40 @@ def addressList(addressPrefix, addressLine1, addressLine2, addressNumber, neighb
 def index(request): 
     return render_to_response('client/client_index.html', {'clientList': Person.objects.all() })
 
-def form(request, client_id=0):
+def form(request, object_id=0):
     try:
     	phones = []
         addresses = []
-        client = get_object_or_404(Person, pk=client_id)
-        #client = get_object_or_404(Client, pk=client_id)
+        object = get_object_or_404(Person, pk=object_id)
+        #client = get_object_or_404(Client, pk=object_id)
         #person= Person.objects.filter( id__exact= client.id ).get( id= client.id )
-        for phone in client.phones.all():
+        for phone in object.phones.all():
             phones.append(phone)
-        for address in client.address.all():
+        for address in object.address.all():
             addresses.append(address)    
     except:
         #client = Client()
-        client= Person()
-    return render_to_response('client/client_form.html', {'object': client, 'phones': phones, 'addresses': addresses } )
+        object= Person()
+    return render_to_response('client/client_form.html', {'object': object, 'phones': phones, 'addresses': addresses } )
 
 
-def save(request, person_id=0):
+def save(request, object_id=0):
     try:
-        person = get_object_or_404(Person, pk=person_id)
+        object = get_object_or_404(Person, pk=object_id)
     except Http404:
-        person = Person()
-    person.name = request.POST['name']
+        object = Person()
+    object.name = request.POST['name']
     #person.nickname = request.POST['nickname']
     #person.photo = request.POST['photo']
     #person.email = request.POST['email']
     if(request.POST['birthDate']):
-        person.birthDate = request.POST['birthDate']
-    person.gender = request.POST['gender']
+        object.birthDate = request.POST['birthDate']
+    object.gender = request.POST['gender']
     #person.maritalStatus = MaritalStatus.objects.get(pk = request.POST['maritalStatus'])
     if(request.POST['birthPlace']):
-        person.birthPlace = City.objects.get(pk = request.POST['birthPlace'])
+        object.birthPlace = City.objects.get(pk = request.POST['birthPlace'])
     
-    person.save() 
+    object.save() 
       
 #    address = Address()
 #    address.addressPrefix = request.POST['addressPrefix']
@@ -89,26 +89,26 @@ def save(request, person_id=0):
 #        person.address.city = City.objects.get(pk= request.POST['city'])
    
    
-    person.address.all().delete()
+    object.address.all().delete()
     for address in addressList(request.POST.getlist('addressPrefix'), request.POST.getlist('addressLine1'), 
                                request.POST.getlist('addressLine2'), request.POST.getlist('addressNumber'),
                                request.POST.getlist('neighborhood'), request.POST.getlist('zipCode'), 
                                request.POST.getlist('addressType'), request.POST.getlist('city'),
                                request.POST.getlist('foreignCountry'), request.POST.getlist('foreignState'),
                                request.POST.getlist('foreignCity')):
-        address.content_object = person
+        address.content_object = object
         address.save()
     
-    person.phones.all().delete()
+    object.phones.all().delete()
     for phone in phoneList(request.POST.getlist('area'), request.POST.getlist('phoneNumber'), request.POST.getlist('ext'), request.POST.getlist('phoneType')):
-        phone.content_object = person
+        phone.content_object = object
         phone.save()
     
     return render_to_response('client/client_index.html')
 
 
-def delete(request, client_id):
-    client = get_object_or_404(Client, pk=client_id)
+def delete(request, object_id):
+    client = get_object_or_404(Client, pk=object_id)
     client.active = False
     client.save()
     return render_to_response('client/client_index.html', {'clientList': Client.objects.all().filter(active = True) })
--- gestorpsi/person/urls.py
+++ gestorpsi/person/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('gestorpsi.person.views',
     (r'^$', 'index'),
     (r'^add$', 'form'),
-    (r'^(?P<client_id>\d+)/$', 'form'),
+    (r'^(?P<object_id>\d+)/$', 'form'),
     (r'^save/$', 'save'),
-    (r'^(?P<client_id>\d+)/save/$', 'save'),
-    (r'^(?P<client_id>\d+)/delete/$', 'delete'),
+    (r'^(?P<object_id>\d+)/save/$', 'save'),
+    (r'^(?P<object_id>\d+)/delete/$', 'delete'),
 )

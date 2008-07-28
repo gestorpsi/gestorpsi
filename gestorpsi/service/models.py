54e5f554bba9ab52b28d3962ae1bf5a0e09f7fa0
vinicius <vinicius@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-07-28 14:30:15 +0000

    Service app. Only the layer model was done. This version of the app should be revised.
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@85 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

---
+++ gestorpsi/service/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+class ServiceType(models.Model):
+    name= models.CharField( max_length= 80 )
+    description= models.CharField( max_length= 100 )
+
+PROCEDURE_LIST= ( ( '1', 'SUS'), ( '2', 'CFP'), ( '3', 'GESTORPSI') )    
+class Procedure(models.Model):
+    id_proc= models.CharField( max_length= 20, blank= True )
+    name= models.CharField( max_length= 80 )
+    description= models.CharField( max_length= 80, blank= True )
+    type= models.CharField( max_length= 1, choices= PROCEDURE_LIST )
+
+class Modality(models.Model):
+    name= models.CharField( max_length= 80 )
+    description= models.CharField( max_length= 100 )
+    
+class GenericArea(models.Model):
+    services= models.ManyToManyField( ServiceType )
+    modalities= models.ManyToManyField( Modality )
+    procedures= models.ManyToManyField( Procedure )
+
+class School(GenericArea):
+    education_modality= models.CharField( max_length= 80 )
+
+class Organizational(GenericArea):
+    hierarchical_level= models.CharField( max_length= 80 )
+
+class Clinic(GenericArea):
+    #FAIXA ETARIA
+    pass
+
+class Agreement(models.Model):
+    name= models.CharField( max_length= 45 )
+    description= models.CharField( max_length= 80 )
+
+class ResearchProject(models.Model):
+    name= models.CharField( max_length= 45 )
+    description= models.CharField( max_length= 80 )
+
+class Service(models.Model):
+    name= models.CharField( max_length= 80 )
+    description= models.CharField( max_length= 100 )
+    keywords= models.CharField( max_length= 100 )
+    agreements= models.ManyToManyField( Agreement )
+    research_project= models.ForeignKey( ResearchProject )
+    #FALTA LIGAR COM ORGANIZATION
+    

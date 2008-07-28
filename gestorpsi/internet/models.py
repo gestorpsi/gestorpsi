63e1cbd7a5d7e2828f8903fa8b6e73ca199fc9eb
vinicius <vinicius@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-07-28 13:30:51 +0000

    Email now has an attribute that represents the 'email type'.
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@82 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/internet/models.py
+++ gestorpsi/internet/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
+
+class EmailType(models.Model):
+    description= models.CharField( max_length= 45 )
+    def __unicode__(self):
+        return self.description
+
 class Email(models.Model):
-    email = models.EmailField(blank=True)
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey()
+    email= models.EmailField(blank=True)
+    email_type= models.ForeignKey( EmailType )
+    content_type= models.ForeignKey(ContentType)
+    object_id= models.PositiveIntegerField()
+    content_object= generic.GenericForeignKey()
     def __unicode__(self):
         return self.email
 

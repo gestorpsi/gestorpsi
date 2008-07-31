a5d015e59f0784816d3b0d599b0dad3a6d68c29c
czd <czd@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-07-31 13:37:49 +0000

    view working until phones. documents now are lists.
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@106 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/client/views.py
+++ gestorpsi/client/views.py
@@ -4,21 +4,33 @@ from django import newforms as forms
 from gestorpsi.client.models import Client
 from gestorpsi.person.models import Person, PersonForm
 from gestorpsi.phone.models import Phone, PhoneType
-from gestorpsi.address.models import Country, City, Address, AddressType
-from gestorpsi.internet.models import Email, EmailType
+from gestorpsi.address.models import Country, City, State, Address, AddressType
+from gestorpsi.internet.models import Email, EmailType, InstantMessenger, IMNetwork
+from gestorpsi.document.models import Document, TypeDocument, Issuer
+
 
 def phoneList(areas, numbers, exts, types):
     total = len(numbers)
-    phones = []
+    objects = []
     for i in range(0, total):
-        if (len(numbers[i])): 
-            phones.append(Phone(area=areas[i], phoneNumber=numbers[i], ext=exts[i], phoneType=PhoneType.objects.get(pk=types[i])))
-    return phones
+        if (len(numbers[i])):
+            objects.append(Phone(area=areas[i], phoneNumber=numbers[i], ext=exts[i], phoneType=PhoneType.objects.get(pk=types[i])))
+    return objects
 
 
+# append documents
+def documentList(typeDocuments, documents, issuers, states):
+    total = len(documents)
+    objects = []
+    for i in range(0, total):
+        if (len(documents[i])):
+            objects.append(Document(typeDocument=TypeDocument.objects.get(pk=typeDocuments[i]), document=documents[i], issuer=Issuer.objects.get(pk=issuers[i]), state=State.objects.get(pk=states[i])))
+    return objects
+
+# append addresses
 def addressList(addressPrefixs, addressLines1, addressLines2, addressNumbers, neighborhoods, zipCodes, addressTypes, city_ids, country_ids, stateChars, cityChars):
     total = len(addressLines1)
-    address = []
+    objects = []
     
     for i in range(0, total):
         if (len(addressLines1[i])):
@@ -27,46 +39,59 @@ def addressList(addressPrefixs, addressLines1, addressLines2, addressNumbers, ne
             #Do jeito que esta ocorrera uma exception  
             if(len(city_ids[i])):
                 #city_id=City.objects.get(pk=city_ids[i])          
-                address.append(Address(addressPrefix=addressPrefixs[i], addressLine1=addressLines1[i], addressLine2=addressLines2[i], 
+                objects.append(Address(addressPrefix=addressPrefixs[i], addressLine1=addressLines1[i], addressLine2=addressLines2[i], 
                                    addressNumber=addressNumbers[i], neighborhood=neighborhoods[i], zipCode=zipCodes[i],
                                    addressType=AddressType.objects.get(pk=addressTypes[i]),                                   
                                    foreignCountry=Country.objects.get(pk=country_ids[i]),
                                    foreignState=stateChars[i],
                                    foreignCity=cityChars[i]))
             else:
-                address.append(Address(addressPrefix=addressPrefixs[i], addressLine1=addressLines1[i], addressLine2=addressLines2[i], 
+                objects.append(Address(addressPrefix=addressPrefixs[i], addressLine1=addressLines1[i], addressLine2=addressLines2[i], 
                                    addressNumber=addressNumbers[i], neighborhood=neighborhoods[i], zipCode=zipCodes[i],
                                    addressType=AddressType.objects.get(pk=addressTypes[i]),
                                    city = City.objects.get(pk=city_ids[i])))                            
             
-    return address
+    return objects
 
 
-
-def index(request): 
+# list objects
+def index(request):
     return render_to_response('client/client_index.html', {'clientList': Person.objects.all() })
 
+# add and edit form
 def form(request, object_id=0):
     try:
         phones = []
         addresses = []
+        documents = []
+        
+        # if exists, get it to edit
         object = get_object_or_404(Person, pk=object_id)        
-        #person= Person.objects.filter( id__exact= client.id ).get( id= client.id )
+
+        # person have phones
         for phone in object.phones.all():
             phones.append(phone)
+        
+        # person have addresses
         for address in object.address.all():
-            addresses.append(address)    
+            addresses.append(address)
+        
+        # person have documents
+        for document in object.document.all():
+            documents.append(document)
+        
     except:
-        #client = Client()
         object= Person()
-    return render_to_response('client/client_form.html', {'object': object, 'phones': phones, 'addresses': addresses, 'countries': Country.objects.all() } )
+        
+    return render_to_response('client/client_form.html', {'object': object, 'phones': phones, 'addresses': addresses, 'countries': Country.objects.all(), 'PhoneTypes': PhoneType.objects.all(), 'AddressTypes': AddressType.objects.all(), 'EmailTypes': EmailType.objects.all(), 'IMNetworks': IMNetwork.objects.all() , 'documents': documents, 'TypeDocuments': TypeDocument.objects.all(), 'Issuers': Issuer.objects.all(), 'States': State.objects.all(), } )
 
-
+# save object
 def save(request, object_id=0):    
     try:
         object = get_object_or_404(Person, pk=object_id)
     except Http404:
         object = Person()
+        
     object.name = request.POST['name']
     object.nickname = request.POST['nickname']
     #person.photo = request.POST['photo']   
@@ -77,17 +102,26 @@ def save(request, object_id=0):
     if(request.POST['birthPlace']):
        object.birthPlace = City.objects.get(pk = request.POST['birthPlace'])
     
+    # save it
     object.save() 
     
-    email = Email()
-    email.email=request.POST['email']
-    if(len(request.POST['email_type'])):
-        email.email_type=EmailType.objects.get(pk=1)
-    else:
-        email.email_type=EmailType.objects.get(pk=request.POST['email_type'])
-    email.content_object = object
-    email.save()   
+    #
+    #email = Email()
+    #email.email=request.POST['email']
+    #if(len(request.POST['email_type'])):
+    #    email.email_type=EmailType.objects.get(pk=1)
+    #else:
+    #    email.email_type=EmailType.objects.get(pk=request.POST['email_type'])
+    #email.content_object = object
+    #email.save()   
     
+    # flush phones and re-insert it
+    object.phones.all().delete()
+    for phone in phoneList(request.POST.getlist('area'), request.POST.getlist('phoneNumber'), request.POST.getlist('ext'), request.POST.getlist('phoneType')):
+        phone.content_object = object
+        phone.save()
+    
+    # flush addresses and re-insert it
     object.address.all().delete()
     for address in addressList(request.POST.getlist('addressPrefix'), request.POST.getlist('addressLine1'), 
                                request.POST.getlist('addressLine2'), request.POST.getlist('addressNumber'),
@@ -98,14 +132,15 @@ def save(request, object_id=0):
         address.content_object = object
         address.save()
     
-    object.phones.all().delete()
-    for phone in phoneList(request.POST.getlist('area'), request.POST.getlist('phoneNumber'), request.POST.getlist('ext'), request.POST.getlist('phoneType')):
-        phone.content_object = object
-        phone.save()
-    
-    return render_to_response('client/client_index.html')
+    # flush documents and re-insert it
+    object.document.all().delete()
+    for document in documentList(request.POST.getlist('document_typeDocument'), request.POST.getlist('document_document'), request.POST.getlist('document_issuer'), request.POST.getlist('document_state')):
+        document.content_object = object
+        document.save()
 
+    return HttpResponse(object.id)
 
+# delete object
 def delete(request, object_id):
     client = get_object_or_404(Client, pk=object_id)
     client.active = False

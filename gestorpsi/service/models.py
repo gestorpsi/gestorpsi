d3eb52bd552abb793be5ce97e5bd2a29528c57ed
vinicius <vinicius@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-08-06 18:05:12 +0000

    App service updated.
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@133 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/service/models.py
+++ gestorpsi/service/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from gestorpsi.organization.models import Organization
+from gestorpsi.careprofessional.models import CareProfessional
 
 class ServiceType(models.Model):
     name= models.CharField( max_length= 80 )
@@ -31,9 +32,13 @@ class Clinic(GenericArea):
     #FAIXA ETARIA
     pass
 
+class AgreementType(models.Model):
+    description= models.CharField( max_length= 80 )
+
 class Agreement(models.Model):
     name= models.CharField( max_length= 45 )
     description= models.CharField( max_length= 80 )
+    agreement_type= models.ForeignKey( AgreementType )
 
 class ResearchProject(models.Model):
     name= models.CharField( max_length= 45 )
@@ -45,6 +50,18 @@ class Service(models.Model):
     keywords= models.CharField( max_length= 100 )
     agreements= models.ManyToManyField( Agreement )
     research_project= models.ForeignKey( ResearchProject )
-    
-    organization = models.ForeignKey(Organization, null=True)    
-    
+    organization= models.ForeignKey(Organization, null=True)
+    responsibles= models.ForeignKey( CareProfessional )
+
+"""
+
+from gestorpsi.service.models import ResearchProject
+research_project= ResearchProject( name= 'a research project', description= 'research project test' )
+research_project.save()
+
+from gestorpsi.service.models import AgreementType
+agreement_type= AgreementType( description= 'agreement type test' )
+
+
+
+"""

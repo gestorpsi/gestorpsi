8aa74755637f5f271e43f88b3b3b105b2e5b7635
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-08-22 14:03:19 +0000

    changes for use new methods for saving addresses, phones and documents
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@175 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/person/views.py
+++ gestorpsi/person/views.py
@@ -1,10 +1,10 @@
 from gestorpsi.address.models import City
-from gestorpsi.address.views import addressSave
-from gestorpsi.document.views import documentSave
-from gestorpsi.phone.views import phoneSave
+from gestorpsi.address.views import address_save
+from gestorpsi.document.views import document_save
+from gestorpsi.phone.views import phone_save
 
 def personSave(request, person):
-
+    # CHECK IF HAS CHANGES BEFORE SAVE
     person.name = request.POST['name']
     person.nickname = request.POST['nickname']
 
@@ -27,17 +27,17 @@ def personSave(request, person):
     person.save()
 
     # save phone numbers (using Phone APP)
-    phoneSave(person, request.POST.getlist('area'), request.POST.getlist('phoneNumber'), request.POST.getlist('ext'), request.POST.getlist('phoneType'))
+    phone_save(person, request.POST.getlist('phoneId'), request.POST.getlist('area'), request.POST.getlist('phoneNumber'), request.POST.getlist('ext'), request.POST.getlist('phoneType'))
 
     # save addresses (using Address APP)
-    addressSave(person, request.POST.getlist('addressPrefix'), request.POST.getlist('addressLine1'),
-                request.POST.getlist('addressLine2'), request.POST.getlist('addressNumber'),
-                request.POST.getlist('neighborhood'), request.POST.getlist('zipCode'), 
-                request.POST.getlist('addressType'), request.POST.getlist('city'),
-                request.POST.getlist('foreignCountry'), request.POST.getlist('foreignState'),
-                request.POST.getlist('foreignCity'))
+    address_save(person, request.POST.getlist('addressId'), request.POST.getlist('addressPrefix'),
+                 request.POST.getlist('addressLine1'), request.POST.getlist('addressLine2'),
+                 request.POST.getlist('addressNumber'), request.POST.getlist('neighborhood'),
+                 request.POST.getlist('zipCode'), request.POST.getlist('addressType'),
+                 request.POST.getlist('city'), request.POST.getlist('foreignCountry'),
+                 request.POST.getlist('foreignState'), request.POST.getlist('foreignCity'))
     
-    # save documents (using Document APP)
-    documentSave(person, request.POST.getlist('document_typeDocument'), request.POST.getlist('document_document'), request.POST.getlist('document_issuer'), request.POST.getlist('document_state'))
+    # save documents (using Document APP) 
+    document_save(person, request.POST.getlist('documentId'), request.POST.getlist('document_typeDocument'), request.POST.getlist('document_document'), request.POST.getlist('document_issuer'), request.POST.getlist('document_state'))
 
     return person

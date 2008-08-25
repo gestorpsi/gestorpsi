2a5e810109b2d606ae906a96f64d2c0a8839af39
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-08-25 19:07:34 +0000

    added cmp functions and audit trail module
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@189 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/internet/models.py
+++ gestorpsi/internet/models.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.contrib import admin
+import audittrail
 
 class EmailType(models.Model):
     description= models.CharField( max_length= 45 )
@@ -15,11 +16,22 @@ class EmailTypeAdmin(admin.ModelAdmin):
 admin.site.register(EmailType, EmailTypeAdmin)
 
 class Email(models.Model):
-    email= models.EmailField(blank=True)
-    email_type= models.ForeignKey( EmailType )
+    email = models.EmailField(blank=True)
+    email_type = models.ForeignKey( EmailType )
+    # Generic Relation
     content_type= models.ForeignKey(ContentType)
     object_id= models.PositiveIntegerField()
     content_object= generic.GenericForeignKey()
+
+    history = audittrail.AuditTrail()
+
+    def __cmp__(self, other):
+        if (self.email == other.email) and \
+           (self.email_type == other.email_type):
+            return 0
+        else:
+            return 1
+
     def __unicode__(self):
         return self.email
 
@@ -30,9 +42,20 @@ class EmailForm(ModelForm):
 class Site(models.Model):
     description = models.CharField(max_length=30, blank=True)
     site = models.URLField(blank=True)
+    # Generic Relation
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
+
+    history = audittrail.AuditTrail()
+
+    def __cmp__(self, other):
+        if (self.description == other.description) and \
+           (self.site == other.site):
+            return 0
+        else:
+            return 1    
+    
     def __unicode__(self):
         return self.site
 
@@ -49,8 +72,19 @@ admin.site.register(IMNetwork, IMNetworkAdmin)
 class InstantMessenger(models.Model):
     identity = models.CharField(max_length=50, blank=True)
     network = models.ForeignKey(IMNetwork, blank=True)
+    # Generic Relation
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey()    
+    content_object = generic.GenericForeignKey()
+    
+    history = audittrail.AuditTrail()
+
+    def __cmp__(self, other):
+        if (self.identity == other.identity) and \
+           (self.network == other.network):
+            return 0
+        else:
+            return 1
+
     def __unicode__(self):
         return "%s (%s)" % (self.identity, self.network)

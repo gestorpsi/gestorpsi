408422597162bdd4cff8d6bbafff10a7a542b368
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-08-25 19:07:55 +0000

    created all funcions that manage internet data
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@190 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/internet/views.py
+++ gestorpsi/internet/views.py
@@ -1,24 +1,99 @@
-# Create your views here.
+from gestorpsi.internet.models import Email, EmailType, Site, InstantMessenger, IMNetwork
 
-
-
-    #email = Email()
-    #email.email=request.POST['email']
-    #if(len(request.POST['email_type'])):
-    #    email.email_type=EmailType.objects.get(pk=1)
-    #else:
-    #    email.email_type=EmailType.objects.get(pk=request.POST['email_type'])
-    #email.content_object = object
-    #email.save()  
-    
-    
-'''
-    email = Email()
-    email.email=request.POST['email']
-    if(len(request.POST['email_type'])):        
-        email.email_type=EmailType.objects.get(pk=1)
+""" ************** Email section ************** """
+def is_equal_email(email):
+    try:
+        email_db = Email.objects.get(pk=email.id)
+    except:
+        return False
+    if cmp(email_db, email) == 0:
+        return True
     else:
-        email.email_type=EmailType.objects.get(pk=request.POST['email_type'])
-    email.content_object = person
-    email.save()
-''' 
+        return False
+
+def email_list(ids, emails, emails_type):
+    objs = []
+    for i in range(0, len(emails)):
+        if (len(emails[i])):            
+            if len(emails_type[i]):
+                et = EmailType.objects.get(pk=emails_type[i])
+            else:
+                et = None
+            objs.append(Email(id=ids[i], email=emails[i], email_type=et))
+    return objs
+
+def email_delete(ids, emails):
+    for i in range(0, len(emails)):
+        if (not len(emails[i]) and len(ids[i])):
+            Email.objects.get(pk=ids[i]).delete()
+
+def email_save(object, ids, emails, emails_type):
+    email_delete(ids, emails)
+    for email in email_list(ids, emails, emails_type):
+        if not is_equal_email(email):
+            email.content_object = object
+            email.save()
+
+""" ************** Site section ************** """
+def is_equal_site(site):
+    try:
+        site_db = Site.objects.get(pk=site.id)
+    except:
+        return False
+    if cmp(site_db, site) == 0:
+        return True
+    else:
+        return False
+
+def site_list(ids, descriptions, sites):
+    objs = []
+    for i in range(0, len(sites)):
+        if (len(sites[i])):
+            objs.append(Site(id=ids[i], description=descriptions[i], site=sites[i]))
+    return objs
+
+def site_delete(ids, sites):
+    for i in range(0, len(sites)):
+        if (not len(sites[i]) and len(ids[i])):
+            Site.objects.get(pk=ids[i]).delete()
+
+def site_save(object, ids, descriptions, sites):
+    site_delete(ids, sites)
+    for site in site_list(ids, descriptions, sites):
+        if not is_equal_site(site):
+            site.content_object = object
+            site.save()
+
+""" ************** Instant Messenger section ************** """
+def is_equal_im(im):
+    try:
+        im_db = InstantMessenger.objects.get(pk=im.id)
+    except:
+        return False
+    if cmp(im_db, im) == 0:
+        return True
+    else:
+        return False
+
+def im_list(ids, identities, networks):
+    objs = []
+    for i in range(0, len(identities)):
+        if (len(identities[i])):            
+            if len(networks[i]):
+                net = IMNetwork.objects.get(pk=networks[i])
+            else:
+                net = None
+            objs.append(InstantMessenger(id=ids[i], identity=identities[i], network=net))
+    return objs
+
+def im_delete(ids, identities):
+    for i in range(0, len(identities)):
+        if (not len(identities[i]) and len(ids[i])):
+            InstantMessenger.objects.get(pk=ids[i]).delete()
+
+def im_save(object, ids, identities, networks):
+    im_delete(ids, identities)
+    for im in im_list(ids, identities, networks):
+        if not is_equal_im(im):
+            im.content_object = object
+            im.save()

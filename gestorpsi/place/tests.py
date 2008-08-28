adad5e7a392eeec21156c029cf953db73992d81f
vinicius <vinicius@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-08-28 15:50:07 +0000

    The class PlaceTest tests how Place's instances are stored on the database. The class ViewPlaceTest which was created in order to test all function views related to Place, Room and others place app. classes is not finished yet; the test testIndex needs some adjusts.
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@205 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

---
+++ gestorpsi/place/tests.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.forms import ModelForm
+from gestorpsi.address.models import Address
+from gestorpsi.phone.models import Phone, PhoneType
+from gestorpsi.place.models import PlaceType, Place
+from gestorpsi.address.models import City, State, Country, AddressType
+from django.contrib.contenttypes import generic
+from gestorpsi.organization.models import Organization
+from django.contrib import admin
+import unittest
+from django.test.client import Client
+
+class PlaceTest(unittest.TestCase):
+    def setUp(self):
+        self.place= Place(label='testing place', visible= True )
+        place_type= PlaceType(description= 'a place type')
+        place_type.save()
+        self.place.place_type= place_type
+        phone_type= PhoneType(description= 'phone type test')
+        phone= Phone(area= '23', phoneNumber= '45679078', ext= '4444', phoneType= phone_type)
+        phone.content_object = self.place
+
+        addressType=AddressType(description='Home')
+        addressType.save()
+        address = Address()
+        address.addressPrefix = 'Rua'
+        address.addressLine1 = 'Rui Barbosa, 1234'
+        address.addressLine2 = 'Anexo II - Sala 4'
+        address.neighborhood = 'Centro'
+        address.zipCode = '12345-123'
+        address.addressType = AddressType.objects.get(pk=1)
+
+        country= Country( name= 'test', nationality= 'testing' )
+        country.save()
+        state= State(name= 'test', shortName= 't', country= country )
+        state.save()
+        city= City(name= 'test', state= state)
+        city.save()
+
+        address.city = city
+        address.content_object = self.place
+
+        self.place.save()
+
+    def testDefaultPlace(self):
+        #get all places stored in the database and put them in a list
+        #then compare the first (and probably only) stored-place with self.place 
+        self.assertEquals(  Place.objects.all()[0], self.place, 'place has not been appropriately saved' )
+
+class ViewPlaceTest( unittest.TestCase ):
+    urls = 'gestorpsi.place.urls'
+    
+    def setUp(self):
+        print 'setup'
+        
+    def testIndex(self):
+        #c= Client()
+        #print "%s" % c.post( '/index/', { 'joaoajoa': 2 } )
+        #self.assertEquals( 200, response.status_code )
+        pass
+
+    def tearDown(self):
+        print 'teardown'
+       
+def suite():
+    suite = unittest.TestLoader().loadTestsFromTestCase( PlaceTest )
+    suite.addTest( ViewPlaceTest('testIndex'))
+    return suite

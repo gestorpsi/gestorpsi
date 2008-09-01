6cf8d0d8f37d11f68b6ebf431ebdfb533fab14ef
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-09-01 17:49:58 +0000

    crypto util
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@228 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

---
+++ gestorpsi/util/CryptographicUtils.py
@@ -0,0 +1,27 @@
+from Crypto.Cipher import Blowfish
+import binascii
+from django.conf import settings
+
+enc_obj= enc_obj= Blowfish.new(settings.SECRET_KEY)
+
+def decrypt_attrib(attrib):
+    """
+    This helper function I{deciphers} C{attrib} and returns it as an unicode string.
+    @type attrib: this variable probably is a C{CharField}
+    @param attrib: the value to be deciphered
+    @author: Vinicius H. S. Durelli
+    """
+    return u'%s' % ( enc_obj.decrypt(binascii.a2b_hex(attrib))).rstrip()
+    
+def encrypt_attrib(attrib):
+    """
+    This helper function I{encrypts} the value passed as parameter and returns the
+    encrypted-value.
+    @type attrib: this variable probably is a C{CharField}
+    @param attrib: the value to be encrypted
+    @author: Vinicius H. S. Durelli
+    """
+    repeat= 8 - (len(attrib) % 8)
+    attrib= attrib + " " * repeat
+    attrib= binascii.b2a_hex( enc_obj.encrypt(attrib))
+    return attrib

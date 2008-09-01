089034033fb9c1e944d2665348e974342a5c7681
vinicius <vinicius@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-09-01 15:16:20 +0000

    All necessary changes to implement cryptographic and universally unique identifier (UUID) were made. It was not necessary to make changes in the 'views' and 'urls' files -- only 'models' was modified.
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@226 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/internet/models.py
+++ gestorpsi/internet/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-
 from django.db import models
 from django.forms import ModelForm
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.contrib import admin
 from gestorpsi.util import audittrail
+from gestorpsi.util.uuid_field import UuidField
+from gestorpsi.util import CryptographicUtils as cryptoUtils
 
 class EmailType(models.Model):
     description= models.CharField( max_length= 45 )
@@ -18,12 +19,21 @@ class EmailTypeAdmin(admin.ModelAdmin):
 admin.site.register(EmailType, EmailTypeAdmin)
 
 class Email(models.Model):
-    email = models.EmailField(blank=True)
+    id= UuidField( primary_key= True )
+    crypt_email = models.CharField( max_length= 232, blank=True)
     email_type = models.ForeignKey( EmailType )
     # Generic Relation
     content_type= models.ForeignKey(ContentType)
     object_id= models.PositiveIntegerField()
     content_object= generic.GenericForeignKey()
+    
+    def _set_email(self, value):
+        self.crypt_email= cryptoUtils.encrypt_attrib( value )
+        
+    def _get_email(self):
+        return cryptoUtils.decrypt_attrib( self.crypt_email )
+    
+    email= property( _get_email, _set_email )
 
     history = audittrail.AuditTrail()
 
@@ -42,12 +52,28 @@ class EmailForm(ModelForm):
         model= Email
 
 class Site(models.Model):
-    description = models.CharField(max_length=30, blank=True)
-    site = models.URLField(blank=True)
+    id= UuidField( primary_key= True )
+    crypt_description = models.CharField(max_length=232, blank=True)
+    crypt_site = models.CharField(max_length=232, blank=True)
     # Generic Relation
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
+    
+    def _set_description(self, value):
+        self.crypt_description= cryptoUtils.encrypt_attrib( value )
+        
+    def _get_description(self):
+        return cryptoUtils.decrypt_attrib( self.crypt_description )
+    
+    def _set_site(self, value):
+        self.crypt_site= cryptoUtils.encrypt_attrib( value )
+        
+    def _get_site(self):
+        return cryptoUtils.decrypt_attrib( self.crypt_site )
+    
+    description= property( _get_description, _set_description )
+    site= property( _get_site, _set_site )
 
     history = audittrail.AuditTrail()
 
@@ -62,7 +88,17 @@ class Site(models.Model):
         return self.site
 
 class IMNetwork(models.Model):
-    description = models.CharField(max_length=30)
+    id= UuidField( primary_key= True )
+    crypt_description = models.CharField(max_length=56)
+    
+    def _set_description(self, value):
+        self.crypt_description= cryptoUtils.encrypt_attrib( value )
+        
+    def _get_description(self):
+        return cryptoUtils.decrypt_attrib( self.crypt_description )
+    
+    description= property( _get_description, _set_description )
+
     def __unicode__(self):
         return self.description
 
@@ -72,12 +108,21 @@ class IMNetworkAdmin(admin.ModelAdmin):
 admin.site.register(IMNetwork, IMNetworkAdmin)
 
 class InstantMessenger(models.Model):
-    identity = models.CharField(max_length=50, blank=True)
+    id= UuidField( primary_key= True )
+    crypt_identity = models.CharField(max_length=160, blank=True)
     network = models.ForeignKey(IMNetwork, blank=True)
     # Generic Relation
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
+    
+    def _set_identity(self, value):
+        self.crypt_identity= cryptoUtils.encrypt_attrib( value )
+        
+    def _get_identity(self):
+        return cryptoUtils.decrypt_attrib( self.crypt_identity )
+    
+    identity= property( _get_identity, _set_identity )
     
     history = audittrail.AuditTrail()
 

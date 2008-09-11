b9e5a513e6213256204a92ea95ac3862a1b02f1b
simone <simone@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-09-11 20:18:14 +0000

    new UID and urls add
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@263 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/device/urls.py
+++ gestorpsi/device/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gestorpsi.device.views',
     ( r'^$', 'index' ),
-    ( r'^form/(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'form' ),
+    ( r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'form' ),
+    (r'^save/$', 'save'),
     (r'^save/(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'save'),
     (r'^delete/(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'delete'), 
 )

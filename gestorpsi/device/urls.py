45c9bd5ef8885acb3ca00e296571d6df20c06a36
simone <simone@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-09-12 18:16:15 +0000

    A new URL has been introduced
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@275 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/device/urls.py
+++ gestorpsi/device/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gestorpsi.device.views',
-    ( r'^$', 'index' ),
-    ( r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'form' ),
-    (r'^save/$', 'save'),
-    (r'^save/(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'save'),
-    (r'^delete/(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'delete'), 
+    (r'^$', 'index'), # list objects
+    (r'^add/$', 'form'), # new object form
+    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'form'), # edit object form
+    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/list_details/$', 'list_details'), #list device details
+    (r'^save/$', 'save'), # save new object
+    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/save/$', 'save'), # update object
+    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/delete/$', 'delete'), # delete object
 )
+

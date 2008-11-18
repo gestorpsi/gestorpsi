145bc9d1986d24cfe8c5b4fdfd946f96051e94b4
czd <czd@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-11-18 16:54:54 +0000

    fixed service active
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@480 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/service/views.py
+++ gestorpsi/service/views.py
@@ -101,7 +101,7 @@ def save(request, object_id = ''):
     object.name = request.POST['service_name']
     object.description = request.POST['service_description']
     object.keywords = request.POST['service_keywords']
-    if request.POST['service_active'] == True:
+    if request.POST['service_active'] == 'True':
         object.active = True
     else:
         object.active = False

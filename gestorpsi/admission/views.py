75b922cd1c07d96b5c8d7ee3c741ebd8d2156904
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-11-28 14:17:54 +0000

    added save_admission and add_relationship functions. Not tested yet !
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@511 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/admission/views.py
+++ gestorpsi/admission/views.py
@@ -14,18 +14,53 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
-from django.conf import settings
-from gestorpsi.client.models import Client
-from gestorpsi.careprofessional.models import LicenceBoard, CareProfessional
+from gestorpsi.client.models import Client, PersonLink, Relation
 from gestorpsi.careprofessional.views import PROFESSIONAL_AREAS
+from gestorpsi.admission.models import *
+from gestorpsi.contact.views import *
 
 def form(request, object_id=''):
-    object    = get_object_or_404(Client, pk=object_id)  
+    object    = get_object_or_404(Client, pk=object_id)
     return render_to_response('admission/admission_form.html', {
+        'address_book_professionals': address_book_get_professionals(request),
+        'address_book_organizations': address_book_get_organizations(request),
         'object': object,
-        'CareProfessionals': CareProfessional.objects.all(),
         'PROFESSIONAL_AREAS': PROFESSIONAL_AREAS,
         'licenceBoardTypes': LicenceBoard.objects.all(),
+        'ReferralChoices': ReferralChoice.objects.all(),
+        'IndicationsChoices': IndicationChoice.objects.all(),
     })
+
+def is_responsible(value):
+    if (value == 'on'):
+        return True
+    else:
+        return False
+
+def add_relationship(object, name_list, relation_list, responsible_list):
+    object.person_link.clear()
+    for i in range(0, len(name_list)):
+        if (len(numbers[i])):
+            object.person_link.add(PersonLink.objects.create(person=Person.objects.create(name=name_list[i]), relation=Relation.objects.get(pk=relation_list[i]), responsible=is_responsible(responsible_list[i])))
+
+    # Deletar
+    #for relation in relation_list:
+    #    person_link = PersonLink()
+    #    person_link.person = Person.objects.create(name=relation[0])
+    #    person_link.relation = Relation.objects.get(pk=relation[1])
+    #    person_link.responsible = relation[2]
+    #    person_link.save()
+    #    object.person_link.add(person_link)
+
+def save_admission(request, object_id=''):
+    object = get_object_or_404(Client, pk=object_id)
+    object.admission_date = request.POST['admission_date']
+    object.idRecord = request.POST['idRecord']
+    object.legacyRecord = request.POST['legacyRecord']
+    object.healthDocument = request.POST['healthDocument']
+    object.comments = request.POST['comments']
+    object.save()
+    add_relationship(object, request.POST.getlist('parent_name'),request.POST.getlist('parent_relation'), request.POST.getlist('parent_responsible'))
+    return HttpResponse('blabla')

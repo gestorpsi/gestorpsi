4d7c62d9317aa333dd757447afe857589775c867
czd <czd@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-12-02 17:57:35 +0000

    changed step title
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@524 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/referral/views.py
+++ gestorpsi/referral/views.py
@@ -23,9 +23,10 @@ from gestorpsi.util.views import date_form_to_db
 
 def form(request, object_id=''):
     user = request.user
-#    object = get_object_or_404(Client, pk=object_id)
+    object = get_object_or_404(Client, pk=object_id)
 
     return render_to_response('referral/referral_form.html', {
+        'object': object,
         'Professionals': CareProfessional.objects.filter(person__organization = user.org_active.id),
         'Services': Service.objects.filter( active=True, organization=user.org_active ),
     })

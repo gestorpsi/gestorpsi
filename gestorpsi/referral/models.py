9d011133e2fa188a2f38342f2cb89a31f043cea8
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-12-09 13:34:04 +0000

    initial referral model commit
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@537 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/referral/models.py
+++ gestorpsi/referral/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+"""
+Copyright (C) 2008 GestorPsi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+from django.db import models
+from gestorpsi.client.models import Client
+from gestorpsi.service.models import Service
+from gestorpsi.careprofessional.models import CareProfessional
+from gestorpsi.util.uuid_field import UuidField
+
+class Referral(models.Model):
+    id = UuidField(primary_key=True)
+    client = models.ForeignKey(Client)
+    professional = models.ForeignKey(CareProfessional, null=True)
+    service = models.ForeignKey(Service, null=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=10, blank=True)
+
+    def __unicode__(self):
+        return u"%s: %s -> %s" % (self.client, self.professional, self.service)

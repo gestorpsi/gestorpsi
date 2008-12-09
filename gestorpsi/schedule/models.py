26a41aab4bb00208771697fc7b72c6dfa8a098ae
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-12-09 13:33:01 +0000

    initial schedule model commit
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@536 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/schedule/models.py
+++ gestorpsi/schedule/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+"""
+Copyright (C) 2008 GestorPsi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+from django.db import models
+from gestorpsi.util.uuid_field import UuidField
+from gestorpsi.referral.models import Referral
+from gestorpsi.place.models import Room
+
+class Schedule(models.Model):
+    id = UuidField(primary_key=True)
+    referral = models.ForeignKey(Referral)
+    appointment_begin = models.DateTimeField()
+    appointment_end = models.DateTimeField()
+    room = models.ForeignKey(Room, null=True)
+
+    def __unicode__(self):
+        return u"Horario: %s - %s (%s)" % (self.appointment_begin, self.appointment_end, self.room)
+

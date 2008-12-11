838382408dc9a06d4da540522b338b73ef9975b4
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2008-12-11 16:24:54 +0000

    added 'save' url for schedule app
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@545 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/schedule/urls.py
+++ gestorpsi/schedule/urls.py
@@ -18,9 +18,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gestorpsi.schedule.views',
     (r'^$', 'index'), # list objects
+    (r'^save/$', 'save'), # save new object
 #    (r'^add/$', 'form'), # new object form
 #    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'form'), # edit object form
-#    (r'^save/$', 'save'), # save new object
 #    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/save/$', 'save'), # update object
 #    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/delete/$', 'delete'), # delete object
 )

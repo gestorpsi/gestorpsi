fa49365d5edff48f344e494ccfb6e601c5cd9d6b
czd <czd@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2009-02-20 19:27:39 +0000

    install script moved to syncdb fixtures
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@579 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/schedule/urls.py
+++ gestorpsi/schedule/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
-urlpatterns = patterns('gestorpsi.schedule.views',
-#    url(r'^$', direct_to_template, { 'template': 'schedule/schedule_index.html'}, name='schedule-home'),
-	(r'^$', include('swingtime.urls')),
-    url(r'^swingtime/events/type/([^/]+)/$', 'event_type', name='karate-event'),
-    (r'^swingtime/', include('swingtime.urls')),
-)
+from gestorpsi.schedule import views
 
+urlpatterns = patterns('gestorpsi.schedule.views',
+    #url(r'^$', direct_to_template, { 'template': 'schedule/schedule_index.html'}, name='schedule-home'),
+#    url(r'^swingtime/events/type/([^/]+)/$', 'event_type', name='karate-event'),
+    url(r'^swingtime/', include('swingtime.urls')),
+)

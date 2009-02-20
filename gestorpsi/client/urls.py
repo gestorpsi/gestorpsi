4691bd31d6a27c7bced5682793ffd2382adce8c8
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2009-02-20 19:48:48 +0000

    added two links for reports
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@587 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/client/urls.py
+++ gestorpsi/client/urls.py
@@ -25,4 +25,6 @@ urlpatterns = patterns('gestorpsi.client.views',
     (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/save/$', 'save'),  #update object
     (r'^(?P<object_id>\d+)/delete/$', 'delete'), # delete object
     (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/delete/$', 'delete'),  #delete object
+    (r'^print/$', 'print_list'), # print client list
+    (r'^(?P<object_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/print/$', 'print_record'),  # print record
 )

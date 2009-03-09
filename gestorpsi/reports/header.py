e72c03f8859b1245a87003c04fbee0eec9d6edba
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2009-03-09 17:37:39 +0000

    Changed report header to fit logo image and the position of some elements.
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@610 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/reports/header.py
+++ gestorpsi/reports/header.py
@@ -25,21 +25,21 @@ def header_gen(organization, header_line=True, clinic_info=True):
     pathdir = '%simg/organization/%s' % (MEDIA_ROOT, organization.id)
     imagefile = os.path.join(PROJECT_ROOT_PATH,pathdir,'.thumb-whitebg',organization.photo)
     class Header(ReportBand):
-        height = 2.3*cm
+        height = 3.4*cm
         borders = {'bottom': False}
         elements = [
-            SystemField(expression='%(report_title)s', top=1*cm, left=0, width=BAND_WIDTH, style={'fontName': 'Helvetica', 'fontSize': 12, 'alignment': TA_CENTER}),
-            SystemField(expression='Printed in %(now:%d/%m/%Y %H:%M)s ', top=1.74*cm, left=0, width=BAND_WIDTH, style={'fontName': 'Helvetica', 'fontSize': 8, 'alignment': TA_LEFT}),
-            SystemField(expression='Page %(page_number)d of %(page_count)d', top=1.74*cm, width=BAND_WIDTH, style={'fontName': 'Helvetica', 'fontSize': 8, 'alignment': TA_RIGHT}),
+            SystemField(expression='%(report_title)s', top=1.9*cm, left=0, width=BAND_WIDTH, style={'fontName': 'Helvetica', 'fontSize': 12, 'alignment': TA_CENTER}),
+            SystemField(expression='Page %(page_number)d of %(page_count)d', top=0.7*cm, width=BAND_WIDTH, style={'fontName': 'Helvetica', 'fontSize': 8, 'alignment': TA_RIGHT}),
+            SystemField(expression='Printed in %(now:%d/%m/%Y %H:%M)s ', top=0.2*cm, width=BAND_WIDTH, style={'fontName': 'Helvetica', 'fontSize': 8, 'alignment': TA_RIGHT}),
         ]
 
         if clinic_info:
-            elements.append(Label(text="%s" % organization, top=0.1*cm, left=0, width=BAND_WIDTH, style={'fontName': 'Helvetica-Bold', 'fontSize': 14, 'alignment': TA_CENTER}))
+            elements.append(Label(text="%s" % organization, top=0.9*cm, left=0, width=BAND_WIDTH, style={'fontName': 'Helvetica-Bold', 'fontSize': 14, 'alignment': TA_CENTER}))
             if len(organization.photo):
                 elements.append(Image(left=0.1*cm, top=0.1*cm, filename=imagefile))
 
         if header_line:
-            elements.append(Line(left=0.0*cm, right=19*cm, top=2*cm, bottom=2*cm))
+            elements.append(Line(left=0.0*cm, right=19*cm, top=3.3*cm, bottom=3.3*cm))
 
     return Header()
 

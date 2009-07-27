3543f3dd44e392c4c45721c208b094f31520e8bb
czd <czd@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2009-07-27 18:45:48 +0000

    schedule: 24 hour format #162
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@1014 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/schedule/forms.py
+++ gestorpsi/schedule/forms.py
@@ -14,13 +14,72 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
 
+from datetime import datetime, date, time, timedelta
 from django import forms
+from django.forms.extras.widgets import SelectDateWidget
 from swingtime.forms import MultipleOccurrenceForm, SingleOccurrenceForm
+from swingtime import utils
 from gestorpsi.schedule.models import ScheduleOccurrence
 from gestorpsi.place.models import Room
 from gestorpsi.device.models import DeviceDetails
+from gestorpsi.schedule import settings as swingtime_settings
+
+
+def timeslot_offset_options(
+    interval=swingtime_settings.TIMESLOT_INTERVAL,
+    start_time=swingtime_settings.TIMESLOT_START_TIME,
+    end_delta=swingtime_settings.TIMESLOT_END_TIME_DURATION,
+    fmt=swingtime_settings.TIMESLOT_TIME_FORMAT
+):
+    '''
+    Create a list of time slot options for use in swingtime forms.
+    
+    The list is comprised of 2-tuples containing the number of seconds since the
+    start of the day and a 12-hour temporal representation of that offset.
+    
+    '''
+    dt = datetime.combine(date.today(), time(0))
+    dtstart = datetime.combine(dt.date(), start_time)
+    dtend = dtstart + end_delta
+    options = []
+
+    delta = utils.time_delta_total_seconds(dtstart - dt)
+    seconds = utils.time_delta_total_seconds(interval)
+    while dtstart <= dtend:
+        options.append((delta, dtstart.strftime('%H:%M')))
+        dtstart += interval
+        delta += seconds
+    
+    return options
+
+default_timeslot_options = timeslot_offset_options()
+default_timeslot_offset_options = timeslot_offset_options()
+
+class SplitDateTimeWidget(forms.MultiWidget):
+    '''
+    A Widget that splits datetime input into a SelectDateWidget for dates and
+    Select widget for times.
+    
+    '''
+    #---------------------------------------------------------------------------
+    def __init__(self, attrs=None):
+        widgets = (
+            SelectDateWidget(attrs=attrs), 
+            forms.Select(choices=default_timeslot_options, attrs=attrs)
+        )
+        super(SplitDateTimeWidget, self).__init__(widgets, attrs)
+
+    #---------------------------------------------------------------------------
+    def decompress(self, value):
+        if value:
+            return [value.date(), value.time().replace(microsecond=0)]
+        
+        return [None, None]
 
 class ScheduleSingleOccurrenceForm(SingleOccurrenceForm):
+    start_time = forms.DateTimeField(widget=SplitDateTimeWidget)
+    end_time = forms.DateTimeField(widget=SplitDateTimeWidget)
+
     room = forms.ModelChoiceField(queryset=Room.objects.all(), widget=forms.Select(attrs={'class':'extramedium asm', }))
     device = forms.ModelMultipleChoiceField(required = False, queryset=DeviceDetails.objects.all(), widget=forms.SelectMultiple(attrs={'class':'extrabig multiple asm', }))
     annotation = forms.CharField(required = False, widget=forms.Textarea())
@@ -30,6 +89,15 @@ class ScheduleOccurrenceForm(MultipleOccurrenceForm):
     room = forms.ModelChoiceField(queryset=Room.objects.all(), widget=forms.Select(attrs={'class':'extramedium asm', }))
     device = forms.ModelMultipleChoiceField(required = False, queryset=DeviceDetails.objects.all(), widget=forms.SelectMultiple(attrs={'class':'extrabig multiple asm', }))
     annotation = forms.CharField(required = False, widget=forms.Textarea())
+    start_time_delta = forms.IntegerField(
+        label='Start time',
+        widget=forms.Select(choices=default_timeslot_offset_options)
+    )
+    
+    end_time_delta = forms.IntegerField(
+        label='End time',
+        widget=forms.Select(choices=default_timeslot_offset_options)
+    )
     
     class Meta:
         model = ScheduleOccurrence

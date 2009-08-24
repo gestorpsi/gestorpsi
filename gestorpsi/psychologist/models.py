7d56c18b593cdb6cbc71f340c9e82ec578caaf70
czd <czd@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2009-08-24 20:01:37 +0000

    added south reverse compatibilty to model
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@1073 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/psychologist/models.py
+++ gestorpsi/psychologist/models.py
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
 
-#from django.db import models
+from django.db import models
 #from gestorpsi.careprofessional.models import CareProfessional
 from gestorpsi.util.uuid_field import UuidField
 

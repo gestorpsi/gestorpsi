a91b2f047fcf13a888b6ca6a624f5c46ee7d9382
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2009-10-09 13:26:54 +0000

    device: fixed a bug when adding/editing a device
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@1251 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/device/views.py
+++ gestorpsi/device/views.py
@@ -168,8 +168,7 @@ def save(request, object_id=None ):
     @type object_id: an instance of the built-in class c{int}.
     """
 
-    object= get_object_or_404(DeviceDetails, pk=object_id, device__organization=request.user.get_profile().org_active) if object_id else DeviceDetails()
-
+    device_details = get_object_or_404(DeviceDetails, pk=object_id, device__organization=request.user.get_profile().org_active) if object_id else DeviceDetails()
     device_details.device = get_object_or_404(Device, pk=request.POST.get('select_device'), organization=request.user.get_profile().org_active)
     device_details.brand = request.POST.get('brand')
     device_details.model = request.POST.get('model')

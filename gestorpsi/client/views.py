f620511c499ffc21c06e811e8b638f3318a5cffc
sergio <sergio@9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5>
2009-10-14 21:29:32 +0000

    referral: partial fix in internal referral
    git-svn-id: svn+ssh://gestorpsi.com.br/usr/home/gestor/svn/gestorpsi/trunk@1265 9d7c2c66-4cfa-11dd-a3c8-a9f1d0faa0c5

--- gestorpsi/client/views.py
+++ gestorpsi/client/views.py
@@ -225,33 +225,27 @@ def add_company(request, object_id=''):
                               )
 
 @permission_required_with_403('referral.referral_read')
-def referral_plus_form(request, object_id = None, referral_id = None):
+def referral_plus_form(request, object_id=None, referral_id=None):
+    """ This function render a form used by internal referral """
     object = get_object_or_404(Client, pk = object_id, person__organization=request.user.get_profile().org_active)
-    referral = get_object_or_404(Referral, pk=referral_id, service__organization=request.user.get_profile().org_active)
-
     data = {'client': [object.id]}
 
-    try:
-        referral = Referral.objects.get(pk=referral_id, service__organization=request.user.get_profile().org_active)
-        referral_form = ReferralForm(instance = referral)
-        referral_list = None
-        referral_form.fields['professional'].queryset = CareProfessional.objects.filter(active=True, person__organization=request.user.get_profile().org_active)
-    except:
-        # new referral
-        referral = ''
-        referral_form = ReferralForm(data)
-
+    referral = Referral.objects.get(pk=referral_id, service__organization=request.user.get_profile().org_active)
+    #referral_form = ReferralForm(instance=referral)
+    referral_form = ReferralForm(data)
+    referral_form.fields['professional'].queryset = CareProfessional.objects.filter(active=True, person__organization=request.user.get_profile().org_active)
     referral_form.fields['referral'].queryset = Referral.objects.filter(client=object)
     referral_form.fields['service'].queryset = Service.objects.filter(active=True, organization=request.user.get_profile().org_active)
     referral_form.fields['client'].queryset = Client.objects.filter(person__organization = request.user.get_profile().org_active.id, clientStatus = '1')
+
     total_service = Referral.objects.filter(client=object).count()
-    referral_list = Referral.objects.filter(client=object, status='01')
+    #referral_list = Referral.objects.filter(client=object, status='01')
 
     return render_to_response('client/client_referral_plus_form.html',
                               {'object': object, 
                                 'referral': referral,
                                 'referral_form': referral_form,
-                                'referral_list': referral_list,
+                                #'referral_list': referral_list,
                                 'referrals': Referral.objects.filter(client = object),
                                 'IndicationsChoices': IndicationChoice.objects.all(),
                                 'contact_organizations': Contact.objects.filter(
@@ -266,9 +260,7 @@ def referral_plus_form(request, object_id = None, referral_id = None):
                                                 filter_name = None,
                                                 filter_type = 2
                                             ),
-                               },
-                              context_instance=RequestContext(request)
-                              )
+                               }, context_instance=RequestContext(request))
 
 
 # add or edit form
@@ -319,29 +311,18 @@ def referral_form(request, object_id = None, referral_id = None):
                               )
 
 @permission_required_with_403('referral.referral_write')
-def referral_plus_save(request, object_id = None, referral_id = None):
+def referral_plus_save(request, object_id=None):
+    """ This function save an internal referral """
     if request.method == 'POST':
-        try:
-            referral = Referral.objects.get(pk=referral_id, service__organization=request.user.get_profile().org_active)
-            form = ReferralForm(request.POST, instance = referral)
-        except:
-            form = ReferralForm(request.POST)
+        form = ReferralForm(request.POST)
         if form.is_valid():
             object = form.save(commit=False)
             object.organization = request.user.get_profile().org_active
             object.status = '01'
+            object.referral = None
             object.save()
+            object.referral_parent.add(Referral.objects.get(pk=request.POST.get('referral')))
             form.save_m2m()
-
-            """ Referral Section """
-            ar = AdmissionReferral()
-            ar.referral_choice = ReferralChoice.objects.get(pk=request.POST.get('referral'))
-            ar.referral_organization = get_object_or_None(Organization, id=request.POST.get('referral_organization'))
-            ar.referral_professional = get_object_or_None(CareProfessional, id=request.POST.get('referral_professional'))
-            ar.client = object
-            ar.save()
-
-
         else:
             print form.errors
     request.user.message_set.create(message=_('Referral saved successfully'))

57e3dccfb4282af065753fc04564f2e7b26029f6
Jayme <contato@doois.com.br>
2012-05-16 14:54:12 -0300

    corrections on the settings

--- gestorpsi/settings.py
+++ gestorpsi/settings.py
@@ -94,8 +94,8 @@ ADMIN_URL = '/admin/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-#ADMIN_MEDIA_PREFIX = '/media_admin/' *************AQUI****************
-ADMIN_MEDIA_PREFIX = '/media_admin/'
+#ADMIN_MEDIA_PREFIX = '/admin_media/' *************AQUI****************
+ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 
 # Make this unique, and don't share it with anybody.

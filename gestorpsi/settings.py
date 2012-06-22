90aaac5d3dc088b0d673decf9f610779d76a605a
Jayme <contato@doois.com.br>
2012-06-22 12:01:29 -0300

    sending the new file return file receiver system

--- gestorpsi/settings.py
+++ gestorpsi/settings.py
@@ -84,13 +84,13 @@ LANGUAGE_COOKIE_NAME = 'gestor_language'
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 #MEDIA_ROOT = '/home/gestor/demo/gestorpsi/media/' *************AQUI****************
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 #MEDIA_URL = 'http://192.168.0.200:8000/media/' *************AQUI****************
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = '/media/'
 
 ADMIN_URL = '/admin/'
 

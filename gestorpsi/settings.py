958857309dabbbf2317694614aed08e2d9623d06
Jayme <kalkehcoisa@gmail.com>
2013-10-28 16:26:07 -0200

    adding configurations to the settings files

--- gestorpsi/settings.py
+++ gestorpsi/settings.py
@@ -25,6 +25,7 @@ SITE_DISABLED = False # display "system is currently under maintenance" message 
 
 ADMINS = (
      ('GestorPsi', 'cuzido@gmail.com'),
+     ('Jayme', 'kalkehcoisa@gmail.com'),
 )
 
 
@@ -216,6 +217,8 @@ INSTALLED_APPS = (
     'paging',
     'indexer',
     'sentry',
+    
+    'packages.paypal.standard.ipn',
 )
 
 STATICFILES_DIRS = (
@@ -270,6 +273,37 @@ djcelery.setup_loader()
 #django celery: task scheduling package
 
 
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         }
+     },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+             'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+
 if not DEBUG:
     import logging
     logging.basicConfig(level=logging.DEBUG)
@@ -279,3 +313,11 @@ if not DEBUG:
     logging.getLogger().addHandler(SentryHandler())
     
     SENTRY_TESTING = True
+
+try:
+    from gestorpsi.settings_local import *
+except:
+    pass 
+
+
+

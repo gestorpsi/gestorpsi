c9ef90e47fecce9098edc043bb3a47d1eabcc30b
Tiago de Souza Moraes <teagom@gmail.com>
2014-05-29 19:34:22 +0000

    Added extra information when a error occur.

---
+++ gestorpsi/extra.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class UserTracebackMiddleware(object):
+
+    """
+        Adds user to request context during request processing, so that they
+        show up in the error emails.
+    """
+    
+    def process_exception(self, request, exception):
+        if request.user.is_authenticated():
+            log = str(request.user.username)
+            log += ';  '
+            log += str(request.user.get_profile().org_active)
+            log += ';  '
+            log += str(request.user.get_profile().org_active.id)
+            request.META['AUTH_USER'] = log
+        else:
+            request.META['AUTH_USER'] = "Anonymous User"

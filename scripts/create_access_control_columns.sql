644ef9a821e3990ddfa4b25db13c1b3ef12a02b7
Carlos Filipe Lima Bezerra <carlosfilipe.lb@gmail.com>
2014-10-27 19:51:28 -0200

    Fixing logic for psychologist attach access
    Signed-of-by: Carlos Filipe Lima Bezerra <carlosfilipe.lb@gmail.com>
    Signed-of-by: Gustavo Correia <correia.gustavol@gmail.com>

--- scripts/create_access_control_columns.sql
+++ scripts/create_access_control_columns.sql
@@ -1,5 +1,11 @@
-alter table referral_referralattach add (is_locked tinyint(1));
+alter table referral_referralattach add (only_professionals tinyint(1));
 
-update referral_referralattach set is_locked = 0;
+update referral_referralattach set only_professionals = 0;
 
-alter table referral_referralattach modify is_locked  tinyint(1) not null;
+alter table referral_referralattach modify only_professionals  tinyint(1) not null;
+
+alter table referral_referralattach add (only_psychologists tinyint(1));
+
+update referral_referralattach set only_psychologists = 0;
+
+alter table referral_referralattach modify only_psychologists  tinyint(1) not null;

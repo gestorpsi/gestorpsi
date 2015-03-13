e21f1d5f649b45f9f529a6e84a1fc9a014887578
Tiago de Souza Moraes <teagom@gmail.com>
2015-03-13 11:22:13 -0300

    Added covenant search tool.

--- gestorpsi/covenant/views.py
+++ gestorpsi/covenant/views.py
@@ -43,10 +43,11 @@ def index(request, active=True):
         covenant index, show all objects, list.
     """
     list_url_base = '/covenant/list/deactive/' if not active else '/covenant/list/active/'
+    tab = 'deactive' if not active else 'active'
 
     return render_to_response('covenant/covenant_index.html',
         {
-            'all' : True,
+            'tab' : tab,
             'list_url_base': list_url_base,
         },
         context_instance=RequestContext(request)
@@ -59,13 +60,11 @@ def form(request, obj=False):
         covenant form, update or save new
         obj: Covenant.id
     """
-
-    add = False
+    tab = 'form' # new register
 
     if obj: # update register
         obj = get_object_or_404(Covenant, pk=obj)
-    else:
-        add = True
+        tab = 'edit' # edit register
 
     if request.POST:
 
@@ -94,6 +93,7 @@ def form(request, obj=False):
 
     # mount form
     else:
+    
         if obj:
             form = CovenantForm( instance=obj )
         else:
@@ -104,7 +104,7 @@ def form(request, obj=False):
                                 {
                                     'form': form,
                                     'obj': obj,
-                                    'add': add,
+                                    'tab': tab,
                                     'category': CATEGORY,
                                     'charge': CHARGE
                                 },
@@ -135,6 +135,13 @@ def list_json(request, active=True):
         obj_list = obj_list.filter(name__startswith=initial).distinct()
         url_extra += '&initial=%s' % initial
 
+        # next and previus letter
+        if ord(initial) < 90:
+            initial_next = chr(ord(initial) + 1)
+
+        if ord(initial) > 65:
+            initial_prev = chr(ord(initial) - 1)
+
     service = request.GET.get('service')
     if service:
         obj_list = obj_list.filter(service__id=service).distinct()
@@ -144,6 +151,8 @@ def list_json(request, active=True):
     if search:
         obj_list = obj_list.filter(name__icontains = search)
         url_extra += '&search=%s' % search
+    else:
+        search = ''
 
     # paginator result
     p = Paginator(obj_list, PAGE_RESULTS)
@@ -156,133 +165,3 @@ def list_json(request, active=True):
     initials = string.uppercase
 
     return render_to_response('tags/list_item_covenant.html', locals(), context_instance=RequestContext(request))
-
-
-
-"""
-    Tiago de Souza Moraes - 06/05/2014
-    retrn: JSON or HTML
-    search person family return JSON
-    search client/initial letter return HTML
-"""
-@permission_required_with_403('client.client_list')
-def list(request, page=1, initial=None, filter=None, no_paging=False, deactive=False, retrn=False):
-
-    user = request.user
-    object_list = Client.objects.from_user(user, 'deactive' if deactive else 'active')
-
-    """
-        return json
-    """
-    if retrn == 'json':
-
-        person = {}
-        i = 0
-
-        for c in object_list.filter(person__name__icontains=filter):
-            person[i] = {
-                'id': c.id, # client id
-                'name': c.person.name,
-            }
-            i = i + 1
-
-        return HttpResponse(simplejson.dumps(person, encoding = 'iso8859-1'), mimetype='application/json')
-
-    """
-        return default
-        client index / paginator
-    """
-    list_url_base = '/client/list/' if not deactive else '/client/list/deactive/'
-
-    url_extra = ''
-    initial = ''
-
-    if request.GET.get('initial'):
-        initial = request.GET.get('initial')
-        
-        if ord(initial) < 90:
-            initial_next = chr(ord(initial) + 1)
-
-        if ord(initial) > 65:
-            initial_prev = chr(ord(initial) - 1)
-
-        object_list = object_list.filter(person__name__istartswith = initial)
-        url_extra += '&initial=%s' % initial
-
-    if request.GET.get('search'):
-        search = request.GET.get('search')
-        object_list = object_list.filter(person__name__icontains = search)
-        url_extra += '&search=%s' % search
-
-    # filters
-    if request.GET.get('service'):
-        service = request.GET.get('service')
-        object_list = object_list.filter(referral__service=service).distinct()
-        url_extra += '&service=%s' % service
-    
-    client_service_pk_list = []
-
-    # subscribed
-    subscribed = True if request.GET.get('subscribed') == 'true' else False
-    if subscribed:
-        if not request.GET.get('service'):
-            object_list = object_list.filter(referral__service__isnull=False).distinct()
-        else:
-            for c in object_list:
-                for r in c.referrals_charged():
-                    if r.service.pk == request.GET.get('service') and c.pk not in client_service_pk_list:
-                        client_service_pk_list.append(c.pk)
-                        break
-        
-        url_extra += '&subscribed=%s' % subscribed
-    
-
-    # discharged
-    discharged = True if request.GET.get('discharged') == "true" else False
-    if discharged:
-        if not request.GET.get('service'):
-            object_list = object_list.filter(referral__referraldischarge__isnull=False).distinct()
-        else:
-            for c in object_list:
-                for r in c.referrals_discharged():
-                    if r.service.pk == request.GET.get('service') and c.pk not in client_service_pk_list:
-                        client_service_pk_list.append(c.pk)
-                        break
-
-        url_extra += '&discharged=%s' % discharged
-
-
-    # queued
-    queued = True if request.GET.get('queued') == "true" else False
-    if queued:
-        queued = request.GET.get('queued')
-        object_list = object_list.filter(referral__queue__isnull=False).distinct()
-            
-        url_extra += '&queued=%s' % queued
-
-
-    # nooccurrences
-    nooccurrences = True if request.GET.get('nooccurrences') == "true" else False
-    if nooccurrences:
-        nooccurrences = request.GET.get('nooccurrences')
-        object_list = object_list.filter(referral__occurrence__isnull=True).distinct()
-        url_extra += '&nooccurrences=%s' % nooccurrences
-
-
-    # service filter
-    if client_service_pk_list: # exclude filtered for charged and discharged results
-        object_list = object_list.filter(pk__in=client_service_pk_list)
-    
-
-    # paginator result
-    p = Paginator(object_list, PAGE_RESULTS)
-    page_number = 1 if not request.GET.get('page') else request.GET.get('page')
-    page = p.page(page_number)
-    object_list = page.object_list
-    
-
-    # mount page
-    service_list = Service.objects.filter( active=True, organization=request.user.get_profile().org_active )
-    initials = string.uppercase
-
-    return render_to_response('tags/list_item.html', locals(), context_instance=RequestContext(request))

a5af7b2bce1474585a4bd6c6efc17ea4defc380b
Beatriz Ferreira <beatriz.216@hotmail.com>
2015-03-31 14:34:19 -0300

    Place admin.py refactored

--- gestorpsi/place/admin.py
+++ gestorpsi/place/admin.py
@@ -19,21 +19,26 @@ GNU General Public License for more details.
    I{These models were created only for testing purposes}
    @author: Sergio Durand
    @version: 1.0
-"""   
+"""
 from django.contrib import admin
 from gestorpsi.place.models import PlaceType, Place, RoomType, Room
+
 
 class PlaceTypeAdmin(admin.ModelAdmin):
     pass
 
+
 class PlaceAdmin(admin.ModelAdmin):
     pass
+
 
 class RoomTypeAdmin(admin.ModelAdmin):
     pass
 
+
 class RoomAdmin(admin.ModelAdmin):
     pass
+
 
 admin.site.register(Place, PlaceAdmin)
 admin.site.register(PlaceType, PlaceTypeAdmin)

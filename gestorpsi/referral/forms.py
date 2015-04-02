4088af546026467eaf8c0d2a08cc448d4271d46b
Beatriz Ferreira <beatriz.216@hotmail.com>
2015-04-02 14:40:01 -0300

    Referral admin.py forms.py refactored

--- gestorpsi/referral/forms.py
+++ gestorpsi/referral/forms.py
@@ -15,54 +15,81 @@ GNU General Public License for more details.
 """
 
 from django import forms
-from gestorpsi.referral.models import Referral, ReferralPriority, ReferralImpact, ReferralDischarge, Queue, ReferralExternal
+from gestorpsi.referral.models import Referral,\
+    ReferralPriority,\
+    ReferralImpact, ReferralDischarge, Queue, ReferralExternal
 from gestorpsi.careprofessional.models import CareProfessional
-from gestorpsi.client.models import Client 
+from gestorpsi.client.models import Client
 from gestorpsi.service.models import Service, ServiceGroup
-
 
 
 '''
     Tiago de Souza Moraes / tiago@futuria.com.br
     update 20 02 2014
 '''
+
+
 class ReferralForm(forms.ModelForm):
 
-    referral = forms.ModelChoiceField(queryset=Referral.objects.all(), required = False, widget=forms.Select(attrs={'class':'extrabig asm', }))
-    service = forms.ModelChoiceField(queryset=Service.objects.all(), widget=forms.Select(attrs={'class':'extrabig asm', }))
-    group = forms.ModelChoiceField(queryset=ServiceGroup.objects.all(), required=False, widget=forms.Select(attrs={'class':'extrabig asm', }))
+    referral = forms.ModelChoiceField(
+        queryset=Referral.objects.all(),
+        required=False,
+        widget=forms.Select(attrs={'class': 'extrabig asm', }))
+    service = forms.ModelChoiceField(
+        queryset=Service.objects.all(),
+        widget=forms.Select(attrs={'class': 'extrabig asm', }))
+    group = forms.ModelChoiceField(
+        queryset=ServiceGroup.objects.all(),
+        required=False, widget=forms.Select(attrs={'class': 'extrabig asm', }))
 
-    #professional = forms.MultipleChoiceField(required=False, widget=forms.CheckboxSelectMultiple, choices = (
-        #[(i.id, i) for i in CareProfessional.objects.all()]
-    #))    
+    # professional = forms.MultipleChoiceField(required=False,
+    # widget=forms.CheckboxSelectMultiple, choices = (
+    # [(i.id, i) for i in CareProfessional.objects.all()]
+    # ))
 
-    client = forms.ModelMultipleChoiceField(queryset=Client.objects.all(), required=False,  widget=forms.SelectMultiple(attrs={'class':'extrabig multiple asm', }))
-    annotation = forms.CharField(widget=forms.Textarea(), required = False)
-    referral_reason = forms.CharField(widget=forms.Textarea(), required = False)
-    available_time = forms.CharField(widget=forms.Textarea(), required = False)
-    priority = forms.ModelChoiceField(queryset=ReferralPriority.objects.all(), required = False, widget=forms.Select(attrs={'class':'extramedium', }))
-    impact = forms.ModelChoiceField(queryset=ReferralImpact.objects.all(), required = False, widget=forms.Select(attrs={'class':'giant', }))
-    
+    client = forms.ModelMultipleChoiceField(
+        queryset=Client.objects.all(),
+        required=False,
+        widget=forms.SelectMultiple(
+            attrs={'class': 'extrabig multiple asm', }))
+    annotation = forms.CharField(widget=forms.Textarea(), required=False)
+    referral_reason = forms.CharField(
+        widget=forms.Textarea(), required=False)
+    available_time = forms.CharField(widget=forms.Textarea(), required=False)
+    priority = forms.ModelChoiceField(
+        queryset=ReferralPriority.objects.all(),
+        required=False, widget=forms.Select(attrs={'class': 'extramedium', }))
+    impact = forms.ModelChoiceField(queryset=ReferralImpact.objects.all(),
+                                    required=False,
+                                    widget=forms.Select(
+                                        attrs={'class': 'giant', }))
+
     class Meta:
-        fields = ('client', 'service', 'professional', 'annotation', 'referral', 'annotation', 'referral_reason', 'available_time', 'priority', 'impact')
+        fields = ('client', 'service', 'professional', 'annotation',
+                  'referral', 'annotation', 'referral_reason',
+                  'available_time', 'priority', 'impact')
         model = Referral
-    
+
     def __init__(self, *args, **kwargs):
         super(ReferralForm, self).__init__(*args, **kwargs)
-        if hasattr(self,'instance') and self.instance.id:
+        if hasattr(self, 'instance') and self.instance.id:
             if self.instance.service.is_group:
             #if self.instance.service.is_group and self.instance.group:
-                self.fields['group'].widget.attrs = {'class':'extrabig asm', 'original_state':self.instance.group.id}
+                self.fields['group'].widget.attrs = {'class': 'extrabig asm',
+                                                     'original_state': self.
+                                                     instance.group.id}
                 self.fields['group'].required = True
-                
 
-        
+
 class ReferralDischargeForm(forms.ModelForm):
-    details = forms.CharField(widget = forms.Textarea(attrs={'class':'giant'}), required = False)
-    description = forms.CharField(widget = forms.Textarea(attrs={'class':'giant'}), required = False)
+    details = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'giant'}), required=False)
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'giant'}), required=False)
 
     class Meta:
-        fields = ('reason', 'was_discussed_with_client', 'details', 'status', 'description', )
+        fields = ('reason', 'was_discussed_with_client', 'details', 'status',
+                  'description', )
         model = ReferralDischarge
 
     def __init__(self, *args, **kwargs):
@@ -70,12 +97,14 @@ class ReferralDischargeForm(forms.ModelForm):
         self.initial.setdefault('referral', self.initial.get('referral', None))
         self.initial.setdefault('client', self.initial.get('client', None))
 
+
 class QueueForm(forms.ModelForm):
     class Meta:
         fields = ('comments', 'client', 'referral')
         model = Queue
 
+
 class ReferralExtForm(forms.ModelForm):
     class Meta:
-        fields = ('comments','referral')
+        fields = ('comments', 'referral')
         model = ReferralExternal

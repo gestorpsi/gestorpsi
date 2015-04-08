7f37cb2d72ceb6923ef7ccb48ea5961bed1a1808
Tiago de Souza Moraes <teagom@gmail.com>
2015-04-08 12:29:30 +0000

    Added set default encoding

--- scripts/createNextInoviceMonthly.py
+++ scripts/createNextInoviceMonthly.py
@@ -7,6 +7,7 @@
 '''
 
 import sys
+import locale
 from os import environ
 
 from dateutil.relativedelta import relativedelta
@@ -25,64 +26,71 @@ from django.core.mail import send_mail
 from gestorpsi.settings import URL_HOME, URL_APP, SIGNATURE
 from gestorpsi.organization.models import Organization 
 from gestorpsi.gcm.models.invoice import Invoice
+from gestorpsi.gcm.models.plan import Plan
 
-import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 # check all invoices that will be expire 10 days from today.
 end = date.today() + timedelta(10) # corret
-#end = date.today() -timedelta(4)  # teste
+print end
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # main code
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 for x in Invoice.objects.filter(end_date=end):
 
-    """
-        contratos
-            - seram avisados um mes antes de vencer
-            - nao gera fatura mensal
+    print x
+
+    if not x.organization.suspension: # suspension false
+
+        """
+            contratos
+                - seram avisados um mes antes de vencer
+                - nao gera fatura mensal
+            
+            boleto 
+                termina em um mês ou menos
+        """
+        i = Invoice() # new invoice
+        i.organization = x.organization
+        i.start_date = end
+        i.end_date = end + relativedelta(months=1)
+        i.payment_type = x.organization.payment_type
+        i.ammount = x.organization.prefered_plan.value
+        i.plan = Plan.objects.get( pk=x.organization.prefered_plan.id )
+        i.status = 0 # pendente
+        i.save()
+
+        to = [] # send mail to
+        # administratror
+        for e in x.organization.administrators_():
+            if not e.profile.user.email in to:
+                to.append(e.profile.user.email) 
+        # secretary
+        for e in x.organization.secretary_():
+            if not e.profile.user.email in to:
+                to.append(e.profile.user.email) 
+
+        # send email
+        """
+            text/body
+            1 = url pagamento
+            2 = data vencimento assinatura atual
+            3 = dia da semana: Segunda-feira
+            4 = URL contato
+            5 = assinatura gestorPSI
+        """
+        w = ['segunda-feira','terça-feira','quarta-feira','quinta-feira','sexta-feira','sábado','domingo']
         
-        boleto 
-            termina em um mês ou menos
-    """
-    i = Invoice() # new invoice
-    i.organization = x.organization
-    i.start_date = end
-    i.end_date = end + relativedelta(months=1)
-    i.payment_type = x.organization.payment_type
-    i.ammount = x.organization.prefered_plan.value
-    i.plan = x.organization.prefered_plan
-    i.status = 0 # pendente
-    i.save()
+        text = u"Bom dia.\n\nSua proxima assinatura já está disponível para pagamento em %s/organization/signature/ Sua assinatura atual vence dia %s, %s, evite ter o seu acesso limitado, pague até esta data.\n\nQualquer dúvida entre em contato pelo link %s/contato/\n" % ( URL_APP , end.strftime("%d %B %Y"), w[date.today().weekday()] , URL_HOME )
+        #text = m.encode('utf-8')
+        text += u"Quer suspender sua assinatura? Clique aqui %s/organization/suspension/\n\n%s" % ( URL_APP, SIGNATURE )
 
-    to = [] # send mail to
-    # administratror
-    for e in x.organization.administrators_():
-        if not e.profile.user.email in to:
-            to.append(e.profile.user.email) 
-    # secretary
-    for e in x.organization.secretary_():
-        if not e.profile.user.email in to:
-            to.append(e.profile.user.email) 
-
-    # send email
-    """
-        text/body
-        1 = url pagamento
-        2 = data vencimento assinatura atual
-        3 = dia da semana: Segunda-feira
-        4 = URL contato
-        5 = assinatura gestorPSI
-    """
-    w = ['segunda-feira','terça-feira','quarta-feira','quinta-feira','sexta-feira','sábado','domingo']
-    
-    text = u"Bom dia.\n\nSua próxima assinatura já está disponível para pagamento em %s/organization/signature/ Sua assinatura atual vence dia %s, %s, evite ter o seu acesso limitado, pague até esta data.\n\nQualquer dúvida entre em contato pelo link %s/contato/\n" % ( URL_APP , end.strftime("%d %B %Y"), w[date.today().weekday()] , URL_HOME )
-
-    text += u"Quer suspender sua assinatura? Clique aqui %s/organization/suspension/\n\n%s" % ( URL_APP, SIGNATURE )
-
-    msg = EmailMessage()
-    msg.subject = u'Assinatura disponível para pagamento - gestorpsi.com.br'
-    msg.body = text
-    msg.to = to
-    msg.send()
+        msg = EmailMessage()
+        msg.subject = u'Assinatura disponível para pagamento - gestorpsi.com.br'
+        msg.body = text
+        msg.to = to
+        msg.send()

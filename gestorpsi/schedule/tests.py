37bb0a41592c675b738a2e6bdb2e3643f37e067c
ristovao <cristovao.frinhani@gmail.com>
2015-04-28 18:13:07 +0000

    Corretion file tests.py with standard unittest and corretion tests

--- gestorpsi/schedule/tests.py
+++ gestorpsi/schedule/tests.py
@@ -1,28 +1,34 @@
 from django.test import TestCase
 from datetime import timedelta
+import unittest
 
 # Determine which specific import should be done.
 from gestorpsi.schedule.views import *
+from gestorpsi.schedule.models import *
 
 
-class ScheduleTest(TestCase):
+class ScheduleTest(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
     def test_start_time_less_than_end_time(self):
         start_time = timedelta(hours=8, minutes=30, seconds=0)
         end_time = timedelta(hours=9, minutes=30, seconds=0)
 
-        self.assertEquals(times_are_invalid(start_time, end_time), False)
+        self.assertEquals(invalid_delta_time(start_time, end_time), False)
 
     def test_start_time_equal_to_end_time(self):
         start_time = timedelta(hours=8, minutes=30, seconds=0)
         end_time = timedelta(hours=8, minutes=30, seconds=0)
 
-        self.assertEquals(times_are_invalid(start_time, end_time), True)
+        self.assertEquals(invalid_delta_time(start_time, end_time), True)
 
     def test_start_time_greater_than_end_time(self):
         start_time = timedelta(hours=9, minutes=30, seconds=0)
         end_time = timedelta(hours=8, minutes=30, seconds=0)
 
-        self.assertEquals(times_are_invalid(start_time, end_time), True)
+        self.assertEquals(invalid_delta_time(start_time, end_time), True)
 
     def test_verify_client(self):
 

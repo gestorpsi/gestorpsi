b71450513bde798b807242d26b9d58f0a6743f60
Levi Moraes <levi.moraes@yahoo.com.br>
2015-04-30 00:51:10 -0300

    Criacao do setup de testes da Modela da classe Client

---
+++ gestorpsi/client/tests.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2008 GestorPsi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+from gestorpsi.client.models import Client
+from gestorpsi.client.models import Person
+from gestorpsi.organization  import organization
+from gestorpsi.person.models import CompanyClient
+import unittest
+
+class ClientTest(unittest.TestCase):
+	
+	def setUp(self):
+		organization = Organization(name='Organizacao Teste',short_name='OT')
+		person = Person(name='Levi Moraes', organization=organization)
+		self.client = Client(person=person)
+
+	def testEmployeeReturn(self):
+		self.assertEquals(self.client.person.name, 'Levi Moraes')

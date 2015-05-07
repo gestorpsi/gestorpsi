d129925f3f116d4d4b05f44ffdfbac662ed56db2
Thaiane <thaiane.unb@hotmail.com>
2015-05-07 15:05:58 -0300

    Adress model test done

--- gestorpsi/address/tests.py
+++ gestorpsi/address/tests.py
@@ -56,20 +56,34 @@ class AddressTypeTest(TestCase):
 
 
 class AddressTest(TestCase):
-	def setUp(self):
-		self.city = City(name="Taguatinga")
-		self.address_type = AddressType(description="Quadra 1")
-		self.address = Address(addressPrefix="DF",addressLine1="Quadra 2",addressNumber="24",
-			addressLine2="Quadra 3",zipCode="72241302",neighborhood="bairro tal",city=self.city,
-			address_type=self.address_type)
-		self.addressPrefix="DF"
-		self.addressLine1 = "Quadra 2"
-		self.addressNumber = "24"
-		self.addressLine2 = "Quadra 3"
-		self.zipCode = "72241302"
-		self.neighborhood = "bairro tal"
+    def setUp(self):
+        country = Country(name='test', nationality='testing')
+        country.save()
+        state = State(name='test', shortName='t', country=country)
+        state.save()
+        city = City(name='test', state=state)
+        city.save()
+        self.city = city
 
-	def testUnicode(self):
-#		expected_result = "%s %s %s %s<br />%s %s %s %s %s (%s)" % (self.addressPrefix, self.addressLine1, self.addressNumber, self.addressLine2, self.zipCode, self.neighborhood, self.city, '' if not hasattr(self.city, 'state') else self.city.state.shortName, '' if not hasattr(self.city, 'state') else self.city.state.country, self.addressType)
-		expected_result = "erro"
+        address_type = AddressType(description="Quadra 1",weight=3)
+        address_type.save()
+        self.address_type = address_type
+
+        self.address = Address(addressPrefix="DF", addressLine1="Quadra 2",
+                               addressNumber="24", addressLine2="Quadra 3",
+                               zipCode="72241302", neighborhood="bairro tal",
+                               city=self.city, addressType=self.address_type)
+        self.addressPrefix="DF"
+        self.addressLine1 = "Quadra 2"
+        self.addressNumber = "24"
+        self.addressLine2 = "Quadra 3"
+        self.zipCode = "72241302"
+        self.neighborhood = "bairro tal"
+
+    def testUnicode(self):
+        #expected_result = "" + self.address.addressPrefix + " " + self.address.addressLine1 + " " + self.address.addressNumber + " " + self.address.addressLine2 + " " + self.address.zipCode + " " + self.address.neighborhood
+        expected_result = u"%s %s %s %s<br />%s %s %s %s %s (%s)" % (\
+            self.address.addressPrefix, self.address.addressLine1, self.address.addressNumber, self.address.addressLine2, \
+            self.address.zipCode, self.address.neighborhood, self.address.city, '' if not hasattr(self.address.city, 'state') else self.address.city.state.shortName, '' if not hasattr(self.address.city, 'state') else self.address.city.state.country, self.address.addressType\
+        )
         self.assertEquals(expected_result, unicode(self.address))

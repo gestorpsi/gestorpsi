f33d99623e0b2c8d3ab1459dad8e9d2f66bf9a7a
TomazMartins <tomaz.unb@outlook.com>
2015-05-11 09:51:14 -0300

    Testing careprofessional models
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

--- gestorpsi/careprofessional/tests.py
+++ gestorpsi/careprofessional/tests.py
@@ -1,6 +1,9 @@
 from gestorpsi.person.models import Person
+from gestorpsi.organization.models import Organization
+from gestorpsi.users.models import User
 from gestorpsi.careprofessional.models import InstitutionType, PostGraduate,\
-    Profession
+    Profession, ProfessionalProfile, CareProfessional, LicenceBoard,\
+    Availability, CareProfessionalManager
 
 from django.test import TestCase
 
@@ -12,7 +15,10 @@ class InstitutionTypeTest(TestCase):
             description="description of the institution")
 
     def testUnicode(self):
-        self.assertEquals(self.description, unicode(self.institution))
+        result = unicode(self.institution)
+        expected_result = self.description
+
+        self.assertEquals(result, expected_result)
 
 
 class PostGraduateTest(TestCase):
@@ -22,7 +28,10 @@ class PostGraduateTest(TestCase):
             description="description of the post graduate")
 
     def testUnicode(self):
-        self.assertEquals(self.description, unicode(self.post_graduate))
+        result = unicode(self.post_graduate)
+        expected_result = self.description
+
+        self.assertEquals(result, expected_result)
 
 
 class ProfessionTest(TestCase):
@@ -33,15 +42,74 @@ class ProfessionTest(TestCase):
         self.profession.type = self.type
 
     def testUnicode(self):
-        self.assertEquals(self.type, unicode(self.profession.type))
+        result = unicode(self.profession.type)
+        expected_result = self.type
+
+        self.assertEquals(result, expected_result)
 
 
-class ProfessionalProfile(TestCase):
+class LicenceBoardTest(TestCase):
     def setUp(self):
-        self.professional_name = "Manuel"
-        pass
+        self.name = "licenses name"
+        self.licences_board = LicenceBoard(name="licenses name")
 
     def testUnicode(self):
-        # TODO: comparing the unicode of the class with the configured in
-        #       setUp().
-        pass
+        result = unicode(self.licences_board)
+        expected_result = self.name
+
+        self.assertEquals(result, expected_result)
+
+
+class StudentProfileTest(TestCase):
+    def setUp(self):
+        self.person = Person(name="tested person")
+        self.care_professional_manager = CareProfessionalManager()
+
+        self.availability = Availability()
+        self.availability.day = "1"
+        self.availability.hour = "2010-03-27"
+
+        self.care_professional = CareProfessional(
+            objects=self.care_professional_manager,
+            availability=self.availability)
+        #self.care_professional.person = self.person
+        #self.care_professional.objects = self.care_professional_manager
+        #self.care_professional.availability = self.availability
+
+        self.professional = self.care_professional
+        self.expected_professional = "tested person"
+
+    def testUnicode(self):
+        result = unicode(self.professional)
+        expected_result = self.expected_professional
+
+        self.assertEquals(result, expected_result)
+
+
+class AvailabilityTest(TestCase):
+    def setUp(self):
+        self.expected_day_hour = "1 - 18:30"
+
+        self.availability = Availability()
+        self.availability.day = "1"
+        self.availability.hour = "18:30"
+
+    def testUnicode(self):
+        result = unicode(self.availability)
+        expected_result = self.expected_day_hour
+
+        self.assertEquals(result, expected_result)
+
+
+class CareProfessional(TestCase):
+    def setUp(self):
+        self.person = Person(name="tested person")
+        self.care_professional_manager = CareProfessionalManager()
+
+        self.availability = Availability
+        self.availability.day = "1"
+        self.availability.hour = "5"
+
+        self.care_professional = CareProfessional(person=self.person,
+            objects=self.care_professional_manager,
+            availability=self.availability)

40f4d8fd5a7a5b6daa6b8d956ee304a55df40f4d
ristovao <cristovao.frinhani@gmail.com>
2015-06-18 18:20:22 +0000

    Corretion for update email in users
    Signed-off-by: cristovao de lima frinhani <cristovao.frinhani@gmail.com>
    Signed-off-by: vinicius rangel <vinirmv23@gmail.com>

---
+++ gestorpsi/media/js/gestorpsi.user.js
@@ -0,0 +1,9 @@
+function jsupdateemail() {
+    
+if (document.getElementsByName("email_mini")[0].value != document.getElementsByName("email_mini_conf")[0].value || document.getElementsByName("email_mini")[0].value == '' || document.getElementsByName("email_mini_conf")[0].value == ''){
+    return;
+}else{
+    document.getElementsByName("email")[0].value=document.getElementsByName("email_mini")[0].value
+}
+    
+}

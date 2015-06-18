07f451212b284092ed3f8c4e83bbcd7e2655b084
ViniciusRangel <vinirmv23@gmail.com>
2015-06-18 18:24:10 -0300

    clean text area
    clean text area in form of change email
    
    Signed-off-by: Vinicius Rangel <vinirmv23@gmail.com>
    cristovao de lima frinhani <cristovao.frinhani@gmail.com>

--- gestorpsi/media/js/gestorpsi.user.js
+++ gestorpsi/media/js/gestorpsi.user.js
@@ -1,9 +1,13 @@
 function jsupdateemail() {
     
 if (document.getElementsByName("email_mini")[0].value != document.getElementsByName("email_mini_conf")[0].value || document.getElementsByName("email_mini")[0].value == '' || document.getElementsByName("email_mini_conf")[0].value == ''){
+    document.getElementsByName("email_mini")[0].value="";
+    document.getElementsByName("email_mini_conf")[0].value="";
     return;
 }else{
     document.getElementsByName("email")[0].value=document.getElementsByName("email_mini")[0].value
+    document.getElementsByName("email_mini")[0].value="";
+    document.getElementsByName("email_mini_conf")[0].value="";
 }
     
 }

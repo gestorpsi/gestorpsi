b7bbcc0fbe4030ab3f1645c277e7d86be4e3e418
Cristóvão L. Frinhani <cristovao.frinhani@gmail.com>
2015-06-19 14:48:03 -0300

    duplication of code refactoring

--- gestorpsi/media/js/gestorpsi.user.js
+++ gestorpsi/media/js/gestorpsi.user.js
@@ -1,13 +1,10 @@
 function jsupdateemail() {
     
 if (document.getElementsByName("email_mini")[0].value != document.getElementsByName("email_mini_conf")[0].value || document.getElementsByName("email_mini")[0].value == '' || document.getElementsByName("email_mini_conf")[0].value == ''){
-    document.getElementsByName("email_mini")[0].value="";
-    document.getElementsByName("email_mini_conf")[0].value="";
-    return;
+
 }else{
-    document.getElementsByName("email")[0].value=document.getElementsByName("email_mini")[0].value
+    document.getElementsByName("email")[0].value=document.getElementsByName("email_mini")[0].value;
+}
     document.getElementsByName("email_mini")[0].value="";
     document.getElementsByName("email_mini_conf")[0].value="";
-}
-    
 }

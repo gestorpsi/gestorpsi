6671c8c3f6e900f91394ddc92ed797d6d52b91b7
Tiago <you@example.com>
2015-08-10 17:21:32 -0300

    Change payment to receive in all scripts.

--- scripts/paymentCovenantFortnightly.py
+++ scripts/paymentCovenantFortnightly.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 '''
-    script to create payment Fortnightly
-    create a new payment for each covenant
+    script to create receive Fortnightly
+    create a new receive for each covenant
 '''
 
 import header
@@ -13,7 +13,7 @@ from datetime import datetime
 from django.db.models.loading import get_models
 loaded_models = get_models()
 
-from gestorpsi.financial.models import Payment
+from gestorpsi.financial.models import Receive
 from gestorpsi.referral.models import Referral
 
 '''
@@ -28,26 +28,26 @@ for r in Referral.objects.filter(covenant__isnull=False, covenant__charge=covena
         filter for all referall that don't have discharge
     '''
     
-    # create a payment for each covenant of referral
+    # create a receive for each covenant of referral
     for c in r.covenant.filter(charge=covenant_charge):
 
         # new
-        payment = Payment()
-        payment.created = datetime
-        payment.name = c.name
-        payment.price = c.price
-        payment.off = 0
-        payment.total = c.price
-        payment.covenant_charge = c.charge
+        receive = Receive()
+        receive.created = datetime
+        receive.name = c.name
+        receive.price = c.price
+        receive.off = 0
+        receive.total = c.price
+        receive.covenant_charge = c.charge
 
         # clear all
-        payment.covenant_payment_way_options = ''
+        receive.covenant_payment_way_options = ''
         for pw in c.payment_way.all():
             x = "(%s,'%s')," % ( pw.id , pw.name ) # need be a dict
-            payment.covenant_payment_way_options += x
+            receive.covenant_payment_way_options += x
 
         # add referral
-        payment.referral = r
+        receive.referral = r
 
         # save
-        payment.save()
+        receive.save()

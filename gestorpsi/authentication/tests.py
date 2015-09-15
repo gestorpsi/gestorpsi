4f5653fdfae8bb144f10c5ea7b2b4404b044ea37
Alessandro Caetano <alessandro.caetanob@gmail.com>
2015-09-15 20:01:55 -0300

    Criando testes da authentication
    Signed-off-by: Alessandro Caetano <alessandro.caetanob@gmail.com>
    Signed-off-by: Leandro Alves <leandrosustenido@gmail.com>

---
+++ gestorpsi/authentication/tests.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2008 GestorPsi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+from django.db import models
+from gestorpsi.person.models import Person
+from gestorpsi.organization.models import Organization
+from django.contrib.auth.models import User, UserManager
+from django.test import TestCase
+from .models import Profile
+
+
+class ProfileTest(TestCase):
+
+    def setUp(self):
+        self.user = models.OneToOneField(User, unique=True)
+        self.organization = models.ManyToManyField(
+            Organization, through='Role', null=True)
+        self.try_login = models.IntegerField(default=0, null=True)
+        self.crypt_temp = models.CharField(
+            max_length=256, blank=True, null=True)
+        self.org_active = models.ForeignKey(
+            Organization, related_name="org_active", null=True)
+        self.person = models.OneToOneField(Person, null=True)
+        self.objects = UserManager()
+
+        self.profile = Profile(user=self.user)
+
+    def testuser(self):
+        self.assertIsInstance(self.user, models.OneToOneField)

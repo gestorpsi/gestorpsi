c67fc49dbaf5cb993ba531ea6d0f22c5c3497c85
Leandro Alves <leandrosustenido@gmail.com>
2015-09-22 17:51:18 -0300

    Testando parametros crypt_temp, org_active, person, unicode, _set_temp e _get_temp
    Signed-off-by: Leandro Alves <leandrosustenido@gmail.com>
    Signed-off-by: Alessandro Caetano <alessandro.caetanob@gmail.com>

--- gestorpsi/authentication/tests.py
+++ gestorpsi/authentication/tests.py
@@ -30,19 +30,15 @@ class ProfileTest(TestCase):
         mcdonalds = Organization()
         papel = Role()
 
-        # self.crypt_temp = "cryptografia"
-        # self.org_active = models.ForeignKey(
-        #     mcdonalds, related_name="org_active", null=True)
-        # self.person = joaquim
-        # self.objects = UserManager()
-
         self.perfil = Profile()
         self.perfil.user = tobias
+        tobias.username = "Tobias"
         self.perfil.pk = 99
-        # self.perfil.crypt_temp = self.crypt_temp
-        # self.perfil.org_active = self.org_active
-        # self.perfil.person = self.person
-        # self.perfil.objects = self.objects
+        mcdonalds.pk = 12
+        joaquim.pk = 44
+        self.perfil.org_active = mcdonalds
+        self.perfil.person = joaquim
+        self.perfil._set_temp("abacate")
 
     def test_user_is_set(self):
         self.assertIsInstance(self.perfil.user, User)
@@ -57,56 +53,20 @@ class ProfileTest(TestCase):
         self.assertFalse(self.perfil.try_login)
 
     def test_crypt_temp_is_set(self):
-        self.assertIsNone(self.perfil.crypt_temp)
+        self.assertIsNotNone(self.perfil.crypt_temp)
 
-    # def test_organization_is_set(self):
-    #     self.assertTrue(issubclass(
-    #         self.perfil.organization, Organization))
+    def test_org_active_is_set(self):
+        self.assertEquals(self.perfil.org_active_id, self.perfil.org_active.pk)
 
-    # def test_organization_params_is_avaible(self):
-    #     self.assertIsNotNone(self.perfil.organization.pk)
+    def test_person_is_set(self):
+        self.assertEquals(self.perfil.person_id, self.perfil.person.pk)
 
-    # def test_try_login_is_set(self):
-    #     self.assertIsNotNone(self.perfil.try_login)
+    def test_unicode(self):
+        self.assertEquals(
+            self.perfil.user.username, unicode(self.perfil))
 
-    # def test_try_login_is_set(self):
-    #     self.assertEquals(self.perfil.try_login, 10)
+    def test_set_temp(self):
+        self.assertEquals(self.perfil.crypt_temp, "adf59a5eebcef0f8")
 
-    # def test_crypt_temp_is_set(self):
-    #     self.assertEquals(self.perfil.crypt_temp, "cryptografia")
-
-    # def test_org_active_is_set(self):
-    #     self.assertIsInstance(self.perfil.org_active, models.ForeignKey)
-
-    # def test_person_is_set(self):
-    #     self.assertIsInstance(self.perfil.person, models.OneToOneField)
-
-    # def test_objects_is_set(self):
-    #     self.assertIsInstance(self.perfil.objects, UserManager)
-
-
-# class RoleTest(TestCase):
-
-#     def setUp(self):
-#         self.tobias = Group
-#         self.joaquim = Profile
-#         self.mcdonalds = Organization
-
-#         self.papel = Role
-
-#         self.papel.profile = models.ForeignKey(self.joaquim)
-#         self.papel.organization = models.ForeignKey(self.mcdonalds)
-#         self.papel.group = models.ForeignKey(self.tobias)
-
-#     def test_profile_is_set(self):
-#         self.assertIsInstance(self.papel.profile, models.ForeignKey)
-
-#     def test_organization_is_set(self):
-#         self.assertIsInstance(self.papel.organization, models.ForeignKey)
-
-#     def test_group_is_set(self):
-#         self.assertIsInstance(self.papel.group, models.ForeignKey)
-
-#     def test_unicode(self):
-#         saida = u"%s | %s | %s" % (self.joaquim, self.mcdonalds, self.tobias)
-#         self.assertEquals(self.papel.__unicode__, saida)
+    def test_get_temp(self):
+        self.assertEquals(self.perfil._get_temp(), "abacate")

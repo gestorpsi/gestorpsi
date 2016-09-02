8fec96e595d16cbfee057268737a5602a6e79960
Tiago <you@example.com>
2016-09-02 18:59:54 -0300

    Moved to install folder.

--- scripts/createadmin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from django.contrib.auth.models import User
-if User.objects.count() == 0:
-    admin = User.objects.create(username='gestorpsi')
-    admin.set_password('123456')
-    admin.is_superuser = True
-    admin.is_staff = True
-    admin.save()

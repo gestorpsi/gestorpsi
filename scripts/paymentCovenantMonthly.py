a69e0f76a9a64fc12ddadd1a37432bf6b805adf1
Tiago <you@example.com>
2016-09-06 16:13:31 -0300

    Fixing scripts and names

--- scripts/paymentCovenantMonthly.py
+++ scripts/paymentCovenantMonthly.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+    Copyright (C) 2008 GestorPsi
+     
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+"""
+
 '''
     script to create receive monthly
     create a new receive for each covenant

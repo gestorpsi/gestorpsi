43536ef637a092103eb39b1d58d357a12fc03c60
Tiago Souza Moraes <teagom@gmail.com>
2016-09-08 19:23:33 +0000

    Added parameters in settings.

--- scripts/invoiceSendMail.py
+++ scripts/invoiceSendMail.py
@@ -25,7 +25,7 @@ import header
 from datetime import date, timedelta
 from django.core.mail import EmailMessage
 
-from gestorpsi.settings import URL_HOME, URL_APP, SIGNATURE, ADMINS_REGISTRATION
+from gestorpsi.settings import URL_HOME, URL_APP, SIGNATURE, ADMINS_REGISTRATION, invoice_check_expiry
 from gestorpsi.gcm.models.invoice import Invoice
 
 def invoice_sendmail():
@@ -33,16 +33,15 @@ def invoice_sendmail():
     # Invoice filter
     invoice_list = [] # list of Invoice
 
-    # check 4 and 7 days before expiry
-    for d in 4, 7:
+    # check days before expiry
+    for d in invoice_check_expiry :
         # check all invoices that will be expire in 10 days.
         expiry = date.today() + timedelta(d) # corret
 
-        #for i in Invoice.objects.filter(expiry_date=expiry, status=0, organization__suspension=False, organization__short_name='IPC'): # debug code
+        print "-- check %s " % expiry
         for i in Invoice.objects.filter(expiry_date=expiry, status=0, organization__suspension=False): # corret code
             if not i in invoice_list:
                 invoice_list.append(i)
-
 
     # for invoice list
     for li in invoice_list:
@@ -67,7 +66,7 @@ def invoice_sendmail():
             3 = URL contato
             4 = assinatura gestorPSI
         """
-        text_body = u"Bom dia.\n\nSua próxima assinatura já está disponível para pagamento em %s/organization/signature/\nVence em %s, evite ter o seu plano suspenso, pague até esta data.\n\nQualquer dúvida entre em contato pelo link %s/contato/\n" % ( URL_APP , li.end_date.strftime("%d %B %Y") , URL_HOME )
+        text_body = u"Bom dia.\n\nSua próxima assinatura já está disponível para pagamento em %s/organization/signature/\nVence em %s, evite ter o seu plano suspenso, pague até esta data.\n\nQualquer dúvida entre em contato pelo link %s/contato/\n" % ( URL_APP , li.expiry_date.strftime("%d %B %Y") , URL_HOME )
         text_body += u"Quer suspender sua assinatura? Clique aqui %s/organization/suspension/\n\n%s" % ( URL_APP, SIGNATURE )
 
         text_subject = u'Assinatura disponível para pagamento - gestorpsi.com.br'

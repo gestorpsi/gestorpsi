109ea370a77e3f80d8cfeb8876a9175f2e9da0c2
Tiago <you@example.com>
2017-03-12 19:45:37 -0300

    Adding internet notify for client and professional.

--- scripts/notifyResumeEvents.py
+++ scripts/notifyResumeEvents.py
@@ -30,58 +30,52 @@ from gestorpsi.gcm.models.invoice import Invoice
 from gestorpsi.schedule.models import *
 
 # check if has events of next day for all professional
-day = date.today() + timedelta(1) # corret
-day = date.today() + timedelta(0) # test
+dt = date.today() + timedelta(1) # correct
+dt = date.today() - timedelta(5) # test
 
 # main code
-d = date.today().day
-m = date.today().month
-y = date.today().year
+d = dt.day
+m = dt.month
+y = dt.year
+
+# sent professional list
+sent = []
 
 # find events of next day
-for o in Occurrence.objects.filter(start_time__year=y, start_time__month=m, start_time__day=d):
-    
+#for o in Occurrence.objects.filter(start_time__year=y, start_time__month=m, start_time__day=d): # correct
+for o in Occurrence.objects.filter(start_time__year=y, start_time__month=m, start_time__day=d, event__referral__organization__id='bcbfdb8e-1641-4478-9e7c-3e0f1befbede'): # test
     """
         profissional quer receber o resumo do dia?
     """
     for p in o.event.referral.professional.all():
-
         events = []
         to = []
 
-        if p.person.notify.all()[0].resume_daily_event:
-            for x in p.person.emails.all():
-                to.append(x)
-            for x in Occurrence.objects.filter(start_time__year=y, start_time__month=m, start_time__day=d, event__referral__professional=c).order_by('-start_time'):
-                events.append(x)
-               
-            #'''
-                #send mail just to user is_active = True
-            #'''
-            ## administratror of org
-            #for e in x.organization.administrators_():
-                #if e.profile.user.is_active and not e.profile.user.email in to:
-                    #to.append(e.profile.user.email) 
+        if p.person.notify.all() and p.person.notify.all()[0].resume_daily_event and p not in sent:
+            sent.append(p) # add professional
 
-            ## secretary of org
-            #for e in x.organization.secretary_():
-                #if e.profile.user.is_active and not e.profile.user.email in to:
-                    #to.append(e.profile.user.email) 
+            for x in p.person.emails.all():
+                to.append(u'%s' % x.email)
 
             # send email
-            """
-                text/body
-                1 = url pagamento
-                2 = data vencimento assinatura atual
-                3 = URL contato
-                4 = assinatura gestorPSI
-            """
-            text = u"Bom dia.\n\nSua próxima assinatura já está disponível para pagamento em %s/organization/signature/\nSua assinatura atual vence dia %s. Evite ter o seu plano suspenso, pague até esta data.\n\nQualquer dúvida entre em contato pelo link %s/contato/\n\n" % ( URL_APP , end.strftime("%d %B %Y, %A") , URL_HOME )
-            text += u"Quer suspender sua assinatura? Clique aqui %s/organization/suspension/\n\n%s" % ( URL_APP, SIGNATURE )
-
-            msg = EmailMessage()
-            msg.subject = u'GestorPsi - Resumo dos eventos'
-            msg.body = text
-            msg.to = to
-            msg.bcc = bcc
-            msg.send()
+            if to:
+                text = "GestorPsi - Resumo dos eventos para o dia %s\n\n" % dt
+                # render occurrence
+                for x in Occurrence.objects.filter(start_time__year=y, start_time__month=m, start_time__day=d, event__referral__professional=p).order_by('-start_time'):
+                    text += "%s - %s\n" % (x.start_time, x)
+               
+                """
+                    text/body
+                    1 = url pagamento
+                    2 = data vencimento assinatura atual
+                    3 = URL contato
+                    4 = assinatura gestorPSI
+                """
+                #text = u"Bom dia.\n\nSua próxima assinatura já está disponível para pagamento em %s/organization/signature/\nSua assinatura atual vence dia %s. Evite ter o seu plano suspenso, pague até esta data.\n\nQualquer dúvida entre em contato pelo link %s/contato/\n\n" % ( URL_APP , end.strftime("%d %B %Y, %A") , URL_HOME )
+                #text += u"Quer suspender sua assinatura? Clique aqui %s/organization/suspension/\n\n%s" % ( URL_APP, SIGNATURE )
+                msg = EmailMessage()
+                msg.to = to
+                msg.body = text
+                msg.subject = u'GestorPsi - Resumo dos eventos'
+                print '---- SENDMAIL '
+                msg.send()

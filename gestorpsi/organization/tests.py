2f9fc99e2f10aed8897ec576cba47e7a59a909ae
laercios <laerciossjr@gmail.com>
2017-05-09 21:12:38 +0000

    Function testShowFormOnGet working

--- gestorpsi/organization/tests.py
+++ gestorpsi/organization/tests.py
@@ -11,7 +11,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager, Group
 from gestorpsi.person.models import Person
 from gestorpsi.phone.models import Phone, PhoneType
 from gestorpsi.organization.models import Organization
@@ -19,8 +19,9 @@ from gestorpsi.internet.models import EmailType, Email
 from django.test.client import Client
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-
-
+from django.test import Client as Cl
+from django.test import TestCase, RequestFactory
+from gestorpsi.util.test_utils import user_stub, setup_required_data
 
 class OrganizationModelTest(TestCase):
     def setUp(self):
@@ -55,23 +56,24 @@ class OrganizationModelTest(TestCase):
     	self.assertFalse(self.organization.clients().exists())
     	#self.assertQuerysetEqual(self.organization.clients(), [])
 
-    '''
-    def testClientExist(self):
-    	User.objects.create(username="user1", password="password")
-        user = User.objects.get(username="user1")
-        Person.objects.create(user_id=user.id)
-        self.person = Person.objects.get(user_id=user.id)
-    	self.assertEqual(Tag.objects.get_for_object(Person.objects.get(pk=1).count(), 1)
-    '''
 class OrganizationViewTest(TestCase):
-	def setUp(self):
-
-		self.client=Client()
-		response = self.client.post('/accounts/login/?next=/', {'username': 'user15', 'password': 'nicepass123'})
+    def setUp(self):
+        self.factory = RequestFactory()
+        # OBS: Using cl instead of client to evade conflicts with Client class
+        self.cl = Cl()
+        setup_required_data()
+        self.cl.post(reverse('registration-register'), user_stub())
+        self.cl.login(
+            username=user_stub()["username"],
+            password=user_stub()["password1"]
+        )
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        req = self.cl.get(reverse('client-index'), user_stub())
+        self.assertEqual(req.status_code, 200)
 
 	def testShowFormOnGet(self):
-		#self.client.login(username='user15', password ='nicepass123')
 		response= self.client.get(reverse('organization-signature'))
-		#print(response)
 		self.assertEquals(200,response.status_code)
-		#self.assertTemplateUsed(response, 'organization_signature.html')
+		self.assertTemplateUsed(response, 'organization_signature.html')

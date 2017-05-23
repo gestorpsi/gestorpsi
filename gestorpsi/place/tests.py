2cbf5038bba8243526a74b7b5eb6e5477b1c4eca
ShutUpPaulo <markes.calado@gmail.com>
2017-05-22 23:07:45 -0300

    Creating Place cases tests

--- gestorpsi/place/tests.py
+++ gestorpsi/place/tests.py
@@ -20,9 +20,96 @@ from gestorpsi.place.models import PlaceType, Place
 from gestorpsi.address.models import City, State, Country, AddressType
 import unittest
 
-class PlaceTest(unittest.TestCase):
+from django.test import TestCase, RequestFactory
+from django.test import Client as Cl
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from gestorpsi.util.test_utils import client_stub, user_stub, setup_required_data, setup_required_service, setup_required_client
+
+class PlaceTests(TestCase):
     def setUp(self):
-        self.place= Place(label='testing place', visible= True )
+        self.factory = RequestFactory()
+        # OBS: Using cl instead of client to evade conflicts with Client class
+        self.client = Cl()
+        setup_required_data()
+        self.name_client = 'Cliente 10'
+
+    def test_client_index_should_redirect_unlogged_user(self):
+        req = self.client.get(reverse('client-index'))
+        self.assertEqual(req.status_code, 302)
+
+    def test_client_index_should_work_for_logged_user(self):
+        # user creation and login
+        self.client.post(reverse('registration-register'), user_stub())
+        res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        req = self.client.get(reverse('client-index'))
+        self.assertEqual(req.status_code, 200)
+
+    def test_show_list_of_places(self):
+        self.client.post(reverse('registration-register'), user_stub())
+        res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        response= self.client.get('/place/')
+        self.assertEquals(200,response.status_code)
+        self.assertTemplateUsed(response, 'place/place_list.html')
+
+    def test_not_show_list_for_unlogged_user(self):
+        self.client.logout() # guarantee that the given user is signed out
+        response= self.client.get('/place/')
+        self.assertEquals(302,response.status_code)
+
+    def test_show_place_add_form(self):
+        self.client.post(reverse('registration-register'), user_stub())
+        res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        response= self.client.get('/place/add/')
+        self.assertEquals(200,response.status_code)
+        self.assertTemplateUsed(response, 'place/place_form.html')
+
+    def test_not_show_form_for_unlogged_user(self):
+        self.client.logout() # guarantee that the given user is signed out
+        response= self.client.get('/place/add/')
+        self.assertEquals(302,response.status_code)
+
+    def test_show_deactive_places(self):
+        self.client.post(reverse('registration-register'), user_stub())
+        res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        response= self.client.get('/place/deactive/')
+        self.assertEquals(200,response.status_code)
+        self.assertTemplateUsed(response, 'place/place_list.html')
+
+    def test_not_show_deactive_places_for_unlogged_user(self):
+        self.client.logout() # guarantee that the given user is signed out
+        response= self.client.get('/place/deactive/')
+        self.assertEquals(302,response.status_code)
+
+    # Foi possível obter o label e o id mas não acessar a rota
+    # def test_show_place(self):
+    #     self.client.post(reverse('registration-register'), user_stub())
+    #     res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+    #     user = User.objects.get(username=user_stub()["username"])
+    #     user.is_superuser = True
+    #     user.save()
+    #     place = Place.objects.get(label='testing place')
+    #     print place.label + ' id: ' + place.id
+    #     response= self.client.get('/place/'+place.id+'/')
+    #     self.assertEquals(200,response.status_code)
+    #     self.assertTemplateUsed(response, 'place/place_form.html')
+
+'''
+class UnitPlaceTest(unittest.TestCase):
+    def setUp(self):
+        self.place= Place(label='testing place')
         place_type= PlaceType(description= 'a place type')
         place_type.save()
         self.place.place_type= place_type
@@ -54,15 +141,15 @@ class PlaceTest(unittest.TestCase):
 
     def testDefaultPlace(self):
         #get all places stored in the database and put them in a list
-        #then compare the first (and probably only) stored-place with self.place 
+        #then compare the first (and probably only) stored-place with self.place
         self.assertEquals(  Place.objects.all()[0], self.place, 'place has not been appropriately saved' )
-
+'''
 class ViewPlaceTest( unittest.TestCase ):
     urls = 'gestorpsi.place.urls'
-    
+
     def setUp(self):
         print 'setup'
-        
+
     def testIndex(self):
         #c= Client()
         #print "%s" % c.post( '/index/', { 'joaoajoa': 2 } )
@@ -71,8 +158,3 @@ class ViewPlaceTest( unittest.TestCase ):
 
     def tearDown(self):
         print 'teardown'
-       
-def suite():
-    suite = unittest.TestLoader().loadTestsFromTestCase( PlaceTest )
-    suite.addTest( ViewPlaceTest('testIndex'))
-    return suite

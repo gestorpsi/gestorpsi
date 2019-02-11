3354d00b3536629d4bd6201beb2eb4d5ef2ea33b
Tiago <teagom@gmail.com>
2019-02-11 11:12:59 -0200

    Adding print receipt payment

--- gestorpsi/schedule/views.py
+++ gestorpsi/schedule/views.py
@@ -1141,3 +1141,42 @@ def schedule_settings(request):
                 tab_settings_class = 'active',
                 places_list = Place.objects.active().filter(organization=request.user.get_profile().org_active.id),
             ), context_instance=RequestContext(request) )
+
+
+def event_print_receipt(event_id=None, request):
+    print event_id
+    object = get_object_or_404( Client, pk=object_id, person__organization=request.user.get_profile().org_active )
+
+    referral = object.referral_set.filter(pk__in=request.POST.getlist('referral'))
+    print_schedule = None if not request.POST.get('schedule') else True
+    print_demographic = None if not request.POST.get('demographic') else True
+    print_ehr = None if not request.POST.get('ehr') else True
+    signed_professional_responsible = None if not request.POST.get('signed_professional_responsible') else True
+    signed_professionals = None if not request.POST.get('signed_professionals') else True
+    signed_organization_reponsibles = None if not request.POST.get('signed_organization_reponsibles') else True
+
+    company_related_clients = []
+    if object.is_company():
+        company_related_clients = CompanyClient.objects.filter(company__person__client = object, company__person__organization=request.user.get_profile().org_active)
+
+        dict = {
+            'referral': referral,
+            'print_schedule': print_schedule,
+            'print_demographic': print_demographic,
+            'signed_professional_responsible': signed_professional_responsible,
+            'signed_professionals': signed_professionals,
+            'signed_organization_reponsibles': signed_organization_reponsibles,
+            'print_ehr': print_ehr,
+            'pagesize' : 'A4',
+            'object': object, 
+            'org_active': request.user.get_profile().org_active, 
+            'user': request.user, 
+            'DEBUG': DEBUG, 
+            'MEDIA_URL': MEDIA_URL if request.POST.get('output') == 'html' else MEDIA_ROOT.replace('\\','/') + '/', 
+            'company_related_clients': company_related_clients,
+            'have_ehr_read_perms': have_ehr_read_perms,
+            'all_payments': request.POST.get('all_payments'),
+            }
+
+    # pdf output format
+    return write_pdf('client/client_print_receipt_payment.html', dict, '%s.pdf' % slugify(object.person.name))
